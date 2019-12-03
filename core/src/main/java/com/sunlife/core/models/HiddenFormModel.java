3df88dffbab4e5663efb5386610046bacd32aebc
yj52 <sudip.chatterjee@sunlife.com>
2019-12-03 16:15:54 +0530

    Adding Button Component and HiddenForm

--- core/src/main/java/com/sunlife/core/models/HiddenFormModel.java
+++ core/src/main/java/com/sunlife/core/models/HiddenFormModel.java
@@ -1,16 +1,21 @@
 package com.sunlife.core.models;
-
 import javax.inject.Inject;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model; 
 
+
 @Model(
 adaptables = {Resource.class},
 defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public interface RichTextModel {
+public interface HiddenFormModel {
 	
 	@Inject
-    String getRichtext();
-}
-
+    String getName();
+	
+	@Inject
+    String getId();
+	
+	@Inject
+	 String getValue();
+	}
