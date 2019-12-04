3ec78cb98e121d6fb21251adc09edeb046470a48
YJ58 <akash.saha@sunlife.com>
2019-12-04 15:14:59 +0530

    layout-container

--- node-app/public/core/js/script.js
+++ node-app/public/core/js/script.js
@@ -1,6 +1,7 @@
 
 
 
+
 // $(document).ready(function() {
 //     $('#locate-advisors-btn').click(function() {
 //         // var form = $('#locate-advisors-form').parsley({});
@@ -11,8 +12,9 @@
 
 // });
 
-
 console.log('component loaded');
+
+console.log('separator loaded');
 $(document).ready(function () {
     $("a.customer-sign-sm").click(function() {
         updateSignInForm('form_signon_mobile');     
@@ -21,12 +23,9 @@ $(document).ready(function () {
           updateSignInForm('form_signon');        
     });
 });     
-console.log('separator loaded');
 console.log('component loaded');
-console.log('image loaded');
 console.log('footer-copyright loaded');
-
+console.log('image loaded');
 console.log('container loaded');
-
 
 console.log('breadcrumb loaded');
