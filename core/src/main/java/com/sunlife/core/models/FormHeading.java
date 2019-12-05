73428ff3bc746ff1e54c8682c9f36881674ba112
yj56 <shivaji.das@sunlife.com>
2019-12-05 21:07:20 +0530

    Created Heading component as per review comments

---
+++ core/src/main/java/com/sunlife/core/models/FormHeading.java
@@ -0,0 +1,27 @@
+package com.sunlife.core.models;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables= {Resource.class},
+		defaultInjectionStrategy=DefaultInjectionStrategy.OPTIONAL
+		)
+public interface FormHeading{
+	
+	@Inject
+	@Named("jcr:title")
+	public String getTitle();
+	
+	@Inject
+	public String getType();
+	
+	@Inject
+	public String getLinkURL();
+	@Inject
+	public String getSelection();
+	
+}
