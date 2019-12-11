8298b1f918a9ffe7255dadad05dc4b7ca03d3038
mo92 <krinil.kumar.shantilal.vaja@sunlife.com>
2019-12-11 00:23:06 -0500

    Region languages meu integration with front end <<Pending>> --Aria --Collpase

---
+++ core/src/main/java/com/sunlife/core/models/RegionLanguageMenu.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.sunlife.core.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+/**
+ * Sling model for regional languages menu
+ * @author MO92
+ */
+@Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class RegionLanguageMenu {
+	// Tab1 Regions
+	@Inject
+	public String title;
+
+	@Inject
+	public List<LinkModel> regions;
+	
+	@Inject
+	public String closeModalText;
+	
+	@Inject
+	public String selectLangText;
+
+	// Tab2 Languages
+	@Inject
+	public String languageSectionTitle;
+
+	@Inject
+	public List<SubLinkModel> languageLinks;
+
+	// Field to expose the processed links to front end
+	private List<List<LinkModel>> regionLinks;
+
+	public List<List<LinkModel>> getRegionLinks() {
+		return regionLinks;
+	}
+
+	public void setRegionLinks(List<List<LinkModel>> regionLinks) {
+		this.regionLinks = regionLinks;
+	}
+
+	/**
+	 * To process data after injection of all injected fields
+	 * @param 
+	 * @return void
+	 * @author MO92
+	 */
+	@PostConstruct
+	public void init() {
+		List<LinkModel> tempList = null;
+		this.regionLinks = new ArrayList<>();
+		for (LinkModel region : regions) {
+			if (tempList == null)
+				tempList = new ArrayList<>();
+			tempList.add(region);
+			if ("yes".equals(region.nextList)) {
+				this.regionLinks.add(tempList);
+				tempList = null;
+			}
+
+			if (regions.indexOf(region) == regions.size() - 1) {
+				this.regionLinks.add(tempList);
+			}
+		}
+	}
+
+}
