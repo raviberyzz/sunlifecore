01c8ea648b1ee0603a6ab81395c739b8d53a1d63
Animesh Kumar <yl20@clarica.com>
2019-12-17 17:53:43 +0530

    merge conflict

--- node-app/public/core/js/script.js
+++ node-app/public/core/js/script.js
@@ -3037,17 +3037,7 @@ function removeParam(key, sourceURL) {
 // } 
 
 
-
-
-function demoFunction()
-{
-alert("Demo-Onsubmit-Function");
-}
-
-console.log('utility-nav loaded');
-
-
-console.log('component loaded');
+console.log('image loaded');
 $(document).ready(function () {
     $("a.customer-sign-sm").click(function() {
         updateSignInForm('form_signon_mobile');     
@@ -3056,8 +3046,19 @@ $(document).ready(function () {
           updateSignInForm('form_signon');        
     });
 });     
-console.log('separator loaded');
 
+
+
+function demoFunction()
+{
+alert("Demo-Onsubmit-Function");
+}
+
+console.log('component loaded');
+console.log('utility-nav loaded');
+
+
+console.log('separator loaded');
 console.log('region-language-menu loaded');
 $(document).ready(function(){
     $('.slf-tab-region .slf-tab').click(function(){
@@ -3066,6 +3067,7 @@ $(document).ready(function(){
     $('.region-present').siblings().css("display", "none");
     $('.language-present').siblings().css("display", "none");
 });
+
 console.log('separator loaded');
 $(document).ready(function () {
     $("#hamburgerMenu").click(function () { 
@@ -3110,10 +3112,8 @@ $(document).ready(function () {
 });     
 console.log('separator loaded');
 console.log('component loaded');
-console.log('image loaded');
-console.log('footer-copyright loaded');
 console.log('icon-text loaded');
-
+console.log('footer-copyright loaded');
 console.log('desktop-header loaded');
 $(document).ready(function(){
     $("#language-btn-container, #sunLanguageCrossBtn").click(function(){
