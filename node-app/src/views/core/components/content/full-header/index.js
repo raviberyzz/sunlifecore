99cb946ae2d7b2ff485bf2d2601f22aa9f80b2b3
YJ58 <akash.saha@sunlife.com>
2019-12-21 15:59:57 +0530

    full integration

--- node-app/src/views/core/components/content/full-header/index.js
+++ node-app/src/views/core/components/content/full-header/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
     $(".signIn-button").attr('maxlength','30');
-    
+    $('#language-btn-container').click(function(){
+     if ($('#sun-search').hasClass('in')){  
+            $("#search-btn").attr('aria-expanded','false');
+            $('#sun-search').removeClass('in');
+        }
+     });
+     $('#search-btn').click(function(){
+        if ($('#sun-language').hasClass('in')){  
+               $("#language-btn").attr('aria-expanded','false');
+               $('#sun-language').removeClass('in');
+           }
+        });
 });
