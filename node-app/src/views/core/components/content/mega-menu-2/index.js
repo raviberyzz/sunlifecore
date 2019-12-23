dfa933204e932579bf182af8550da59978646995
YJ58 <akash.saha@sunlife.com>
2019-12-23 19:03:45 +0530

    full integration

--- node-app/src/views/core/components/content/mega-menu-2/index.js
+++ node-app/src/views/core/components/content/mega-menu-2/index.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    $( ".mega-menu2-list ul" ).find( ".dropdown-submenu" ).hover(
-        function(){
-           // ("#slf-header-mega-menu2").height('1000px');
-            $('#slf-header-mega-menu2').css()
-    }, function(){
-       $("#explore1").removeClass("box-class");
-       }
-    );
-   });
+// $(document).ready(function(){
+//     $( ".mega-menu2-list ul" ).find( ".dropdown-submenu" ).hover(
+//         function(){
+//            // ("#slf-header-mega-menu2").height('1000px');
+//             $('#slf-header-mega-menu2').css()
+//     }, function(){
+//        $("#explore1").removeClass("box-class");
+//        }
+//     );
+//    });
