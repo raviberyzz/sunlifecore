0b4e7b3c4b1ebed292c369aaf09ed6ec612ec384
YJ57 <suchana.mitra@sunlife.com>
2020-01-06 15:45:41 +0530

    aem css upload

--- node-app/src/views/core/components/content/right-navigation/index.js
+++ node-app/src/views/core/components/content/right-navigation/index.js
@@ -30,13 +30,13 @@ $(document).ready(function () {
     if (cta_index==0)
     {
         right_nav_width(child);
-        maxheight(child,0,0);
+        //maxheight(child,0,0);
     }
     else if (cta_index==1)
     {
        var child2=child-1;
        right_nav_width(child2);
-       maxheight(child2,0,0);
+       //maxheight(child2,0,0);
        firstfull();
       
     }
@@ -44,7 +44,7 @@ $(document).ready(function () {
     { 
         var childbefore= cta_index-1;
         right_nav_width(childbefore);
-        maxheight(childbefore,0,0);
+        //maxheight(childbefore,0,0);
         var childafter=child-childbefore;
         for (var i=cta_index-1; i<child; i++)
         {
@@ -94,90 +94,168 @@ $(document).ready(function () {
     function firstfull(){
         $('.right-nav-cta-orange').removeClass('col-sm-6 col-sm-4').addClass('col-sm-12');
     }
-    function maxheight( child1,maxHeight1,maxHeight2)
+    if (( $(window).width() <1025 &&  $(window).width() >767 ))
     {
+        maxheight(child, cta_index);
+    }
+    // function maxheight( child1,maxHeight1,maxHeight2)
+    // {
 
-        if (( $(window).width() <1025 &&  $(window).width() >767 )) {
-                    var maxHeight = -1;
-                    if (child1 < 4)
-                    {
-                        comp.each(function() {
-                            maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+    //     if (( $(window).width() <1025 &&  $(window).width() >767 )) {
+    //                 var maxHeight = -1;
+    //                 if (child1 < 4)
+    //                 {
+    //                     comp.each(function() {
+    //                         maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
                            
-                          });
-                          comp.each(function() {
-                            $(this).height(maxHeight);
-                          });
-                    }
-                    else
-                    {
-                        if (child1%2==0)
-                        {
-                            i=0;
-                            i1=0;
-                            j=(child1/2);
-                            j1=(child1/2);
-                            comp.each(function() {
-                                if (i <(child1/2)) {
-                                    maxHeight1 = maxHeight1 > $(comp[i]).height() ? maxHeight1 : $(comp[i]).height();
-                                    i++;
-                                }
+    //                       });
+    //                       comp.each(function() {
+    //                         $(this).height(maxHeight);
+    //                       });
+    //                 }
+    //                 else
+    //                 {
+    //                     if (child1%2==0)
+    //                     {
+    //                         i=0;
+    //                         i1=0;
+    //                         j=(child1/2);
+    //                         j1=(child1/2);
+    //                         comp.each(function() {
+    //                             if (i <(child1/2)) {
+    //                                 maxHeight1 = maxHeight1 > $(comp[i]).height() ? maxHeight1 : $(comp[i]).height();
+    //                                 i++;
+    //                             }
                                 
-                            });
-                            comp.each(function() {
-                                if (i1 <(child1/2)) {
-                                $(comp[i1]).height(maxHeight1);
-                                i1++;
-                                }
-                            });
-                            comp.each(function() {                               
-                                if (j <child1) {
-                                maxHeight2 = maxHeight2 > $(comp[j]).height() ? maxHeight2 : $(comp[j]).height();
-                                j++;
-                                }
-                            });
-                            comp.each(function() {
-                                if (j1 <child1) {
-                                $(comp[j1]).height(maxHeight2);
-                                j1++;
-                                }
-                            });
-                         }
+    //                         });
+    //                         comp.each(function() {
+    //                             if (i1 <(child1/2)) {
+    //                             $(comp[i1]).height(maxHeight1);
+    //                             i1++;
+    //                             }
+    //                         });
+    //                         comp.each(function() {                               
+    //                             if (j <child1) {
+    //                             maxHeight2 = maxHeight2 > $(comp[j]).height() ? maxHeight2 : $(comp[j]).height();
+    //                             j++;
+    //                             }
+    //                         });
+    //                         comp.each(function() {
+    //                             if (j1 <child1) {
+    //                             $(comp[j1]).height(maxHeight2);
+    //                             j1++;
+    //                             }
+    //                         });
+    //                      }
+    //                     else
+    //                     {
+    //                         i=0;
+    //                         i1=0;
+    //                         j=3;
+    //                         j1=3;
+    //                         comp.each(function() {
+    //                             if (i <3) {
+    //                                 maxHeight1 = maxHeight1 > $(comp[i]).height() ? maxHeight1 : $(comp[i]).height();
+    //                                 i++;
+    //                             }
+                                
+    //                         });
+    //                         comp.each(function() {
+    //                             if (i1 <3) {
+    //                             $(comp[i1]).height(maxHeight1);
+    //                             i1++;
+    //                             }
+    //                         });
+    //                         comp.each(function() {                               
+    //                             if (j <5) {
+    //                             maxHeight2 = maxHeight2 > $(comp[j]).height() ? maxHeight2 : $(comp[j]).height();
+    //                             j++;
+    //                             }
+    //                         });
+    //                         comp.each(function() {
+    //                             if (j1 <5) {
+    //                             $(comp[j1]).height(maxHeight2);
+    //                             j1++;
+    //                             }
+    //                         });
+    //                     }
+    //                 }
+    //             }
+    // }
+ 
+    function maxheight(child, cta_index)
+    {
+        var maxHeight=0;
+        var height1=0;
+        var height2=0;
+        if (cta_index==0)
+        {
+            if (child < 4)
+            {
+                comp.each(function() {
+                    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+                });
+                comp.each(function() {
+                    $(this).height(maxHeight);
+                });
+            }
+            else
+            {
+                if (child==5)
+                {
+                    
+                    var row1=$('.right-navigation-wrapper').children().filter(function(){return $(this).hasClass('col-sm-4')});
+                    var row2=$('.right-navigation-wrapper').children().filter(function(){return $(this).hasClass('col-sm-6')});
+                    row1.each(function() {
+                        height1 =height1 > $(this).height() ? height1 : $(this).height();
+                       // alert(height1);
+                       // alert($(this).height());
+                    });
+                    row1.each(function() {
+                        $(this).height(height1+30);
+                    });
+                    row2.each(function() {
+                        height2 = height2 > $(this).height() ? height2 : $(this).height();
+                      alert(height2);
+                    });
+                    row2.each(function() {
+                        $(this).height(height2);
+                    });
+
+                }
+                else
+                {
+                    var row1_child=child/2;
+                    comp.each(function(index) {
+                        if (index < row1_child)
+                        {
+                           
+                            var height1 = height1 > $(this).height() ? height1 : $(this).height();
+                            alert(height1);
+                        }
                         else
                         {
-                            i=0;
-                            i1=0;
-                            j=3;
-                            j1=3;
-                            comp.each(function() {
-                                if (i <3) {
-                                    maxHeight1 = maxHeight1 > $(comp[i]).height() ? maxHeight1 : $(comp[i]).height();
-                                    i++;
-                                }
-                                
-                            });
-                            comp.each(function() {
-                                if (i1 <3) {
-                                $(comp[i1]).height(maxHeight1);
-                                i1++;
-                                }
-                            });
-                            comp.each(function() {                               
-                                if (j <5) {
-                                maxHeight2 = maxHeight2 > $(comp[j]).height() ? maxHeight2 : $(comp[j]).height();
-                                j++;
-                                }
-                            });
-                            comp.each(function() {
-                                if (j1 <5) {
-                                $(comp[j1]).height(maxHeight2);
-                                j1++;
-                                }
-                            });
+                            var height2 = height2 > $(this).height() ? height2 : $(this).height();
                         }
-                    }
+                        
+                    });
+                    comp.each(function() {
+                        if (index < row1_child)
+                        {
+                            $(this).height(height1);
+                        }
+                        else
+                        {
+                            $(this).height(height1);
+                        }
+                       
+                    });
+
                 }
+            }
+            
+        }
+        
     }
- 
  });
  
