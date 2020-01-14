f507152be1fe70a11e0d62eec8592599e0a06ddc
Animesh Kumar <yl20@clarica.com>
2020-01-14 13:53:42 +0530

    footer accessibility

--- node-app/src/views/core/components/content/link-list/index.js
+++ node-app/src/views/core/components/content/link-list/index.js
@@ -1,23 +1,8 @@
-// $(document).ready(function () {
-// 	$('footer .accordion-heading').click(function () {
-// 		$(this).siblings('.list-div').toggle('collapse');
-// 		$(this).parent().parent().parent().siblings().children().children().children('.list-div').css('display', 'none');
-// 		$(this).parent().parent().parent().siblings().children().children().children('.list-div').siblings('h3').attr('aria-expanded', false);
-// 		if ($(this).attr('aria-expanded') == 'true') {
-// 			$(this).attr('aria-expanded', false);
-// 		}
-// 		else if ($(this).attr('aria-expanded') == 'false') {
-// 			$(this).attr('aria-expanded', true);
-// 		}
-// 	});
-// });
-
-/* Node app js*/
 $(document).ready(function () {
-	$('.accordion-heading').click(function () {
+	$('footer .accordion-heading').click(function () {
 		$(this).siblings('.list-div').toggle('collapse');
-		$(this).parent().parent().siblings().children().children('.list-div').css('display', 'none');
-		$(this).parent().parent().siblings().children().children('.list-div').siblings('h3').attr('aria-expanded', false);
+		$(this).parent().parent().parent().siblings().children().children().children('.list-div').css('display', 'none');
+		$(this).parent().parent().parent().siblings().children().children().children('.list-div').siblings('h3').attr('aria-expanded', false);
 		if ($(this).attr('aria-expanded') == 'true') {
 			$(this).attr('aria-expanded', false);
 		}
@@ -25,16 +10,31 @@ $(document).ready(function () {
 			$(this).attr('aria-expanded', true);
 		}
 	});
-	$('.links .accordion-heading').keyup(function (event) {
-        if (event.keyCode == 13) {
-			if($(this).attr('aria-expanded')=='false'){
-			$(this).attr('aria-expanded','true');
-			$(this).siblings().css('display','block');
-			}
-			else{
-				$(this).attr('aria-expanded','false');
-				$(this).siblings().css('display','none');
-		}
-	  }
-	});
 });
+
+/* Node app js*/
+// $(document).ready(function () {
+	// $('.accordion-heading').click(function () {
+	// 	$(this).siblings('.list-div').toggle('collapse');
+	// 	$(this).parent().parent().siblings().children().children('.list-div').css('display', 'none');
+	// 	$(this).parent().parent().siblings().children().children('.list-div').siblings('h3').attr('aria-expanded', false);
+	// 	if ($(this).attr('aria-expanded') == 'true') {
+	// 		$(this).attr('aria-expanded', false);
+	// 	}
+	// 	else if ($(this).attr('aria-expanded') == 'false') {
+	// 		$(this).attr('aria-expanded', true);
+	// 	}
+	// });
+	// $('.links .accordion-heading').keyup(function (event) {
+    //     if (event.keyCode == 13) {
+	// 		if($(this).attr('aria-expanded')=='false'){
+	// 		$(this).attr('aria-expanded','true');
+	// 		$(this).siblings().css('display','block');
+	// 		}
+	// 		else{
+	// 			$(this).attr('aria-expanded','false');
+	// 			$(this).siblings().css('display','none');
+	// 	}
+	//   }
+// 	});
+// });
