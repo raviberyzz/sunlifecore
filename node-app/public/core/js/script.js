b22caccb3191c089fd6d2b02d78ce41cb785cc68
Animesh Kumar <yl20@clarica.com>
2020-01-27 16:23:49 +0530

    site level accessibility

--- node-app/public/core/js/script.js
+++ node-app/public/core/js/script.js
@@ -4153,16 +4153,16 @@ $(document).ready(function () {
    $va = "<div " + "class= " +'"fa fa-info-circle"' + "> </div>";
     $($va).insertBefore(".tool-tip-box");
    
-    $('.tool-tip-box').css('display','none');
-    $('.fa-info-circle').css('position','initial');
-    $('.fa-info-circle').hover(function(){
-        $('.fa-info-circle').css('cursor','pointer');
-        $('.fa-info-circle').css('position','absolute');
-        $('.tool-tip-box').css('display','block');
-    },function(){
-        $('.fa-info-circle').css('position','initial');
-        $('.tool-tip-box').css('display','none');
-    });
+    // $('.tool-tip-box').css('display','none');
+    // $('.fa-info-circle').css('position','initial');
+    // $('.fa-info-circle').hover(function(){
+    //     $('.fa-info-circle').css('cursor','pointer');
+    //     $('.fa-info-circle').css('position','absolute');
+    //     $('.tool-tip-box').css('display','inline-block');
+    // },function(){
+    //     $('.fa-info-circle').css('position','initial');
+    //     $('.tool-tip-box').css('display','none');
+    // });
 });
 
     
