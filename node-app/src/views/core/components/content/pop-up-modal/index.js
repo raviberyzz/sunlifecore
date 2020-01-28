5709ee05280d8bbf3bee7272fe843867298023e8
YJ58 <akash.saha@sunlife.com>
2020-01-28 13:09:21 +0530

    heading changes

--- node-app/src/views/core/components/content/pop-up-modal/index.js
+++ node-app/src/views/core/components/content/pop-up-modal/index.js
@@ -7,23 +7,16 @@ $(document).ready(function(){
       $(window).resize(function() {
             popUpWidth();
       });
-      /* For Link listComponent */
-      $('.popup-modal-wrapper .accordion-heading').click(function () {
-		$(this).siblings('.list-div').toggle('collapse');
-		$(this).parent().parent().parent().siblings().children().children().children('.list-div').css('display', 'none');
-		$(this).parent().parent().parent().siblings().children().children().children('.list-div').siblings('h3').attr('aria-expanded', false);
-		if ($(this).attr('aria-expanded') == 'true') {
-			$(this).attr('aria-expanded', false);
-		}
-		else if ($(this).attr('aria-expanded') == 'false') {
-			$(this).attr('aria-expanded', true);
-		}
-  });
   // $('.popup-modal-wrapper').each(function(){
   //   var $modal = $(this);
   //   var id = $(this).attr('id');
   //   $('a[href="#'+id+'"]').click(function(){
   //     $modal.show();
   //   });
+  // });
+  // $('.primary-blue-button').click(function(){
+  //   //$('#modal').addClass('in');
+  //   $('#modal').css({'display':'block'});
+  //   $('body').addClass('modal-open');
   // });
 });
