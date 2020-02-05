3381f856d3ddb0b8c44a6662c624af762dd4fb9c
Aishik Deb <yl21@clarica.com>
2020-02-05 16:59:59 +0530

    life moment style update

--- node-app/public/core/js/script.js
+++ node-app/public/core/js/script.js
@@ -4154,6 +4154,16 @@ $('.cmp-accordion__button,.cmp-accordion__panel').mousedown(function(e) {
 });
 /* Accordion accessibility ends here */
 });
+$(document).ready(function () {
+	$('.site-level-notification .close-div').click(function () {
+		$('.site-level-notification').css('display', 'none');
+	});
+	$('.site-level-notification .close-div').keyup(function (event) {
+		if (event.keyCode == 13) {
+			$('.site-level-notification').css('display', 'none');
+		}
+	});
+});
 
 
 
@@ -4287,16 +4297,6 @@ function stickyHeader(){
 }
 $(stickyHeader);
 $(window).resize(stickyHeader);
-$(document).ready(function () {
-	$('.site-level-notification .close-div').click(function () {
-		$('.site-level-notification').css('display', 'none');
-	});
-	$('.site-level-notification .close-div').keyup(function (event) {
-		if (event.keyCode == 13) {
-			$('.site-level-notification').css('display', 'none');
-		}
-	});
-});
 $(document).ready(function(){
       var popHeight=$(window).height();
       $(".subscribe-popup-wrapper").height(popHeight);
@@ -4936,22 +4936,23 @@ $(document).ready(function () {
     if(pageWidth>=768){
         $('.blue-background-wrapper').find('.list-unstyled').children('li').children('a').each(function(){
             var text=$(this).text();
-            var words=text.split(" ");
-            var index=text.indexOf(words[2]);
+            var lastWord=text.split(" ").pop();
+            var index=text.indexOf(lastWord);
             text=text.substr(0,index);
             $(this).text("");
-            words[2]=words[2].toUpperCase();
-            var html="<span>"+text+"</span>"+"<br><strong>"+words[2]+"</strong>";
+            lastWord=lastWord.toUpperCase();
+            var html="<span>"+text+"</span>"+"<br><strong>"+lastWord+"</strong>";
             $(this).append(html);
         });
         $('.blue-background-wrapper p:last').css('margin-bottom','16px');
         $('.yellow-background-wrapper').find('.list-unstyled').children('li').children('a').each(function(){
             var text=$(this).text();
-            var words=text.split(" ");
-            var index=text.indexOf(words[2]);
+            var lastWord=text.split(" ").pop();
+            var index=text.indexOf(lastWord);
             text=text.substr(0,index);
             $(this).text("");
-            var html="<span>"+text+"</span>"+"<br><strong>"+words[2]+"</strong>";
+            lastWord=lastWord.toUpperCase();
+            var html="<span>"+text+"</span>"+"<br><strong>"+lastWord+"</strong>";
             $(this).append(html);
         });
         $('.yellow-background-wrapper p:last').css('margin-bottom','16px');
