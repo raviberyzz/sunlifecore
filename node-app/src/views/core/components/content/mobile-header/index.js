82ff05b85f498456b6116a36d73d890b3d6962e7
DE48 <Sri.Harsha@sunlife.com>
2020-02-19 11:41:34 -0500

    js fix for hamburger menu

--- node-app/src/views/core/components/content/mobile-header/index.js
+++ node-app/src/views/core/components/content/mobile-header/index.js
@@ -13,8 +13,7 @@ $(document).ready(function () {
         $('.container-component').css({'margin-left':'270px'});
         $('body').addClass('overflow-hidden');
         $('.slf-mobile-header-wrapper').css({'position':'static'});
-        var windowHeight=$(window).height();
-        $('.hamburger-menu-wrapper').height(windowHeight);
+
     });
     $("#close-hamburger").click(function () {
         $('.hamburger-menu-wrapper').removeClass('active').addClass('inactive');      
@@ -60,17 +59,17 @@ $(document).ready(function () {
             }
         mobileLogoWidth();
         if ($(window).width() > 1024) {
-            $('.container-component').css({'margin-left':'0'});
+            $('.container').css({'margin-left':'0'});
             $('body').removeClass('overflow-hidden'); 
         }
         else if ($(window).width() < 1025) {
             if ($('.hamburger-menu-wrapper').hasClass('active')){
-                $('.container-component').css({'margin-left':'270px'});
+                $('.container').css({'margin-left':'270px'});
                 $('body').addClass('overflow-hidden');
                 $('.slf-mobile-header-wrapper').css({'position':'static'});
             }
             else{
-                $('.container-component').css({'margin-left':'0'}); 
+                $('.container').css({'margin-left':'0'}); 
                 $('body').removeClass('overflow-hidden');
                 $('.slf-mobile-header-wrapper').css({'position':'fixed'}); 
             } 
