467e5d1ccfa73fee86a44eb888f20a03c887655c
MO93 <senthil.kumar.tamilarasan@sunlife.com>
2020-03-03 16:09:26 -0500

    Changed the logic for including header and footer dynamically in experience fragment model

--- core/src/main/java/ca/sunlife/web/cms/core/models/ExperienceFragmentModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/ExperienceFragmentModel.java
@@ -2,24 +2,39 @@ package ca.sunlife.web.cms.core.models;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.jcr.RepositoryException;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.day.cq.wcm.api.Page;
+import ca.sunlife.web.cms.core.services.SiteConfigService;
 
 @Model(adaptables = { SlingHttpServletRequest.class,
 		Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ExperienceFragmentModel {
+	
+    /** The Constant LOGGER. */
+	private static final Logger log = LoggerFactory.getLogger(ExperienceFragmentModel.class);
 	
 		
 	/** The Fragment Path. */
 	@Inject
 	@Via("resource")
 	private String fragmentPath;
+	
+	
+	/** The config service. */
+	@Inject
+	private SiteConfigService configService;
 	
 	private String modifiedFragmentPath;	
 	
@@ -43,10 +58,11 @@ public class ExperienceFragmentModel {
 	}
 
 	@PostConstruct
-	public void init()  {		
+	public void init()  {	
 		
-		String pageLocale = currentPage.getLanguage().getLanguage(); 
-		String currentLocale = currentPage.getLanguage().toString(); 
+		String headerPath = "";
+		String fragmentSplit = "";		
+		
 		
 		if( null == fragmentPath )
 		{
@@ -56,16 +72,36 @@ public class ExperienceFragmentModel {
 			modifiedFragmentPath = fragmentPath;
 		}
 		else
-		{
-		String country = currentLocale.split("_")[1];
-		country = StringUtils.lowerCase(country);
-	
-		if(fragmentPath.contains(country)&&(fragmentPath.contains("header")||fragmentPath.contains("footer"))) {
-			String localeToBReplaced=fragmentPath.substring(fragmentPath.indexOf(country)).split("/")[1];
-			modifiedFragmentPath=fragmentPath.replace("/"+localeToBReplaced+"/", "/"+pageLocale+"/");
-		}else {
-			modifiedFragmentPath=fragmentPath;
-		}
+		{			
+			if(fragmentPath.contains("header")||fragmentPath.contains("footer")) {
+			try {
+				headerPath = configService.getConfigValues("experienceFragmentPath", currentPage.getPath());	
+				log.info("Header path is" + headerPath);
+				if( null != headerPath && headerPath.length() > 0 ) {				
+				String[] pathSplit = fragmentPath.split("/");
+				for(int i=0; i<pathSplit.length;i++) {
+        		if(pathSplit[i].contains("header") || pathSplit[i].contains("footer")) {
+        			fragmentSplit = pathSplit[i];
+        			fragmentSplit = "/" + fragmentSplit + "/";
+        				break;
+        				}
+				}
+				String[] finalSplit = fragmentPath.split(fragmentSplit);
+				modifiedFragmentPath = headerPath + fragmentSplit + finalSplit[1];
+				}
+				else {
+					modifiedFragmentPath=fragmentPath;
+				}
+				}
+			 catch (Exception e) {
+				 log.error("Error :: init method of Experience fragment model :: {}", e);
+				}
+			
+			}
+			else {
+				modifiedFragmentPath=fragmentPath;		
+			}
+
 		}
 	  }
 	}
