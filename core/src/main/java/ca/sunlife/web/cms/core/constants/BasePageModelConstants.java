a71221a1461e1bddf41f1e11291b513577034215
Your Name <you@example.com>
2020-03-03 14:17:43 -0500

    Conntants file

---
+++ core/src/main/java/ca/sunlife/web/cms/core/constants/BasePageModelConstants.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ca.sunlife.web.cms.core.constants;
+
+/**
+ * @author mo92
+ * The Class BasePageModelConstants
+ */
+public class BasePageModelConstants {
+
+	/**
+	 * Constructor BasePageModelConstants
+	 */
+	private BasePageModelConstants() {}
+
+	public static final String SLASH_CONSTANT = "/";
+}
