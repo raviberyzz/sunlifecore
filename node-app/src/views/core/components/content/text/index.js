7a4743192a761c6d61c948b4a406cfa78c8a226d
Aishik Deb <yl21@clarica.com>
2020-03-03 21:07:43 +0530

    region language update

--- node-app/src/views/core/components/content/text/index.js
+++ node-app/src/views/core/components/content/text/index.js
@@ -1,57 +1,57 @@
 $(document).ready(function () {
-  var windowsize = $( window ).width();
+  var windowsize = $(window).width();
   //faCircle();
-$(".cmp-text,th,td").each(function(){
-    var tool=$(this).find(".tool-tip-box:first");
-var $va = "<a href='javascript:void(0)' title='' " + "class= " +'"fa fa-info-circle"' + "> </a>";
-  $($va).insertBefore(tool);
-    var tool_content= "";
-    if($(this).attr('class')==='cmp-text'){
-     $(this).children().children(".tool-tip-box").each(function(){
-  tool_content = tool_content +"<p>"+ $(this).html()+"</p>";
-     });
+  $(".cmp-text,th,td").each(function () {
+    var tool = $(this).find(".tool-tip-box:first");
+    var $va = "<a href='javascript:void(0)' title='' " + "class= " + '"fa fa-info-circle"' + "> </a>";
+    $($va).insertBefore(tool);
+    var tool_content = "";
+    if ($(this).attr('class') === 'cmp-text') {
+      $(this).children().children(".tool-tip-box").each(function () {
+        tool_content = tool_content + "<p>" + $(this).html() + "</p>";
+      });
     }
-    else{
-    $(this).children(".tool-tip-box").each(function(){
-  tool_content = tool_content +"<p>"+ $(this).html()+"</p>";
+    else {
+      $(this).children(".tool-tip-box").each(function () {
+        tool_content = tool_content + "<p>" + $(this).html() + "</p>";
+      });
+    }
+    $('.fa-info-circle').attr('data-toggle', 'tooltip');
+    if (windowsize > 767) {
+      $('.fa-info-circle').removeAttr('data-placement');
+      $('.fa-info-circle').attr('data-placement', 'right');
+    } else {
+      $('.fa-info-circle').removeAttr('data-placement');
+      $('.fa-info-circle').attr('data-placement', 'bottom');
+    }
+
+    $('.fa-info-circle').attr('data-html', 'true');
+    if ($(this).attr('class') === 'cmp-text') {
+      $(this).find('a').attr('data-original-title', tool_content);
+    }
+    else {
+      $(this).children('a').attr('data-original-title', tool_content);
+    }
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.fa-info-circle').click(function () {
+      $('.fa-info-circle').css('text-decoration', 'none');
     });
-    }
-     $('.fa-info-circle').attr('data-toggle','tooltip');
-    if(windowsize>767){
-            $('.fa-info-circle').removeAttr('data-placement');
-          $('.fa-info-circle').attr('data-placement','right');
-        } else {
-            $('.fa-info-circle').removeAttr('data-placement');
-          $('.fa-info-circle').attr('data-placement','bottom');
-        }
 
-     $('.fa-info-circle').attr('data-html','true');
-    if($(this).attr('class')==='cmp-text'){
-     $(this).find('a').attr('data-original-title',tool_content);
-    }
-    else{
-    $(this).children('a').attr('data-original-title',tool_content);
-    }
-     $('[data-toggle="tooltip"]').tooltip();
-     $('.fa-info-circle').click(function(){
-         //$('.tooltip-inner').css('display','block');
-       $('.fa-info-circle').css('text-decoration','none');
-     });
+  });
 
-});
-
-  /*function faCircle(){
-        if(windowsize>767){
-            //$('.fa-info-circle').removeAttr('data-placement');
-          $('.fa-info-circle').attr('data-placement','right');
-        } else {
-            ///$('.fa-info-circle').removeAttr('data-placement');
-          $('.fa-info-circle').attr('data-placement','bottom');
-        }
-    }*/
-  $( window ).resize( function () {
-      //$('.tooltip-inner').css('display','none');
-  windowsize = $( window ).width();
-      faCircle();
+  function faCircle() {
+    $('[data-toggle="tooltip"]').tooltip();
+    if (windowsize > 767) {
+      //$('.fa-info-circle').removeAttr('data-placement');
+      $('.fa-info-circle').attr('data-placement', 'right');
+    } else {
+      ///$('.fa-info-circle').removeAttr('data-placement');
+      $('.fa-info-circle').attr('data-placement', 'bottom');
+    }
+  }
+  $(window).resize(function () {
+    $('[data-toggle="tooltip"]').tooltip('destroy');
+    windowsize = $(window).width();
+    faCircle();
   });
 });
