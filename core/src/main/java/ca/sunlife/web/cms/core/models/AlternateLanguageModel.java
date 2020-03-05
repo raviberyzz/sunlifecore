68f4c696370eb5ab3b1f8c1ed7c1db44d39b749d
Your Name <you@example.com>
2020-03-05 16:49:04 -0500

    Fields level doc added

--- core/src/main/java/ca/sunlife/web/cms/core/models/AlternateLanguageModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/AlternateLanguageModel.java
@@ -11,20 +11,40 @@ import org.apache.sling.models.annotations.Model;
 
 /**
  * @author mo92
- *
+ * The Interface AlternateLanguageModel
  */
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public interface AlternateLanguageModel {
 	
+	/**
+	 * Gets the language code.
+	 *
+	 * @return language code
+	 */
 	@Inject
 	String getLanguageCode();
 	
+	/**
+	 * Gets the site location.
+	 *
+	 * @return site location
+	 */
 	@Inject
 	String getSiteLocation();
 	
+	/**
+	 * Gets the domain.
+	 *
+	 * @return domain
+	 */
 	@Inject
 	String getDomain();
 	
+	/**
+	 * Gets if default language.
+	 *
+	 * @return if default language
+	 */
 	@Inject
 	String getDefaultLanguage();
 
