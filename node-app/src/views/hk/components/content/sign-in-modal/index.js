d71f496c7241ed62ea628609d708bb9638d44acc
YJ58 <akash.saha@sunlife.com>
2020-03-05 18:44:13 +0530

    sign in modal

---
+++ node-app/src/views/hk/components/content/sign-in-modal/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("a.customer-sign-sm").click(function() {
+        updateSignInForm('form_signon_mobile');     
+      });  
+    $('#signin-widget-modal').on('shown.bs.modal', function() {
+          updateSignInForm('form_signon');        
+    });
+    function modalWidth(){
+      var winWidth=$(window).width();
+      $("#mySignInModal").width(winWidth);
+      $("#mySignInModal").addClass('horizontal-middle-align');
+    }
+    if ($(window).width() > 1024) {
+      modalWidth();
+    }
+    $(window).resize(function() {
+      if ($(window).width() > 1024) {
+        modalWidth();
+      }
+    }); 
+}); 
