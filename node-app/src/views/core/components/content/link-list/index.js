f551f13138f9bee9ee084f9f24c311edb85566c9
DE48 <Sri.Harsha@sunlife.com>
2020-04-14 18:22:58 -0400

    css changes for embed editorial navigation and title component

--- node-app/src/views/core/components/content/link-list/index.js
+++ node-app/src/views/core/components/content/link-list/index.js
@@ -28,7 +28,41 @@ $(document).ready(function () {
 			$(this).parent().addClass("selectedlink");
 		}
 	})
+   // editorial navigation logic starts here
+   function mobileEditorial(){
+		if($(window).width() < 768){
+			if($(".editorial-nav-desktop-wrapper").length){
+				if( $(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div form").length){
+				$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div form").remove();
+				}
+			console.log("working2");
+				var items=[];
+				var mobileNavigation = "<form><div><label class='sr-only'>Select a topic</label><select></select></div><div><button><span class='fa fa-chevron-right'></span></button></div></form>";
+				$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div .list-unstyled").after(mobileNavigation);
+				$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div .list-unstyled li").each(function(index){
+				items.push($(this).text());
 
+			})
+				for (i=0;i<items.length;i++){
+
+					var option = "<option></option>";
+					var options = $(".links .list-div form select").append(option);
+					var options = $(".links .list-div form select option");
+					options[i].append(items[i]);
+				}
+		    } 
+
+			$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div .list-unstyled").css("display","none");
+			$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div form div:first-child").addClass('Editorial-form-input');
+			$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div form div:nth-child(2)").addClass('primary-blue-button-form Editorial-form-button');
+
+		} else{
+		  $(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div .list-unstyled").css("display","inline-flex");
+			$(".editorial-nav-desktop-wrapper .lists-wrapper .links .list-div form").css("display", "none");
+		}
+    }
+    mobileEditorial();
+    $(window).resize(mobileEditorial);
 
 });
 
