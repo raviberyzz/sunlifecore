dadbb90360f2995ecfd007f464af56d4536e9f37
Your Name <you@example.com>
2020-04-23 15:08:30 -0400

    Made changed for data-value, field desc changes

--- node-app/src/views/core/components/form/options/index.js
+++ node-app/src/views/core/components/form/options/index.js
@@ -20,6 +20,22 @@ $(document).ready(function () {
 
             });
         }
+		
+		var dataCollectionAttr = selectObj.attr("data-collection");
+        if( null != dataCollectionAttr && '' != dataCollectionAttr && 'true' == dataCollectionAttr ) {
+			//Un-bind change event if isOnChangeRequired is not enabled
+            if( !(null != isOnChangeRequired && '' != isOnChangeRequired && 'yes' == isOnChangeRequired) ) {
+				$(selectObj).unbind("change");
+            }
 
+            //Bind on change event to dropdown
+            $(selectObj).bind("change", function(e) {
+                e.preventDefault();
+
+                var dataValue = $(selectObj[0]).val();
+                $(selectObj).attr("data-value", dataValue);
+
+            });
+        }
     });
 });
