0514440ec8bb0c21a61784fe7601eabe8fc6e05a
Your Name <you@example.com>
2020-04-30 18:28:25 -0400

    notification level dismiss with cookie included

--- node-app/src/views/core/components/content/site-level-notification/index.js
+++ node-app/src/views/core/components/content/site-level-notification/index.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
-	$('.site-level-notification .close-div').click(function () {
-		createCookie("pageNotification","true",1,false);
-		$('.site-level-notification').css('display', 'none');
-	});
+	$('.site-level-notification .close-div').click(function() {
+		var siteNotification = this.closest(".site-level-notification");
+        var siteNotificationDiv = $(siteNotification).find(".cmp-container");
+        var siteNotificationId = siteNotificationDiv.attr('id');
+		createCookie("pageNotification-"+siteNotificationId,"true",1,false);
+        $(siteNotification).css('display', 'none');
+    });
 	$('.site-level-notification .close-div').keyup(function (event) {
 		if (event.keyCode == 13) {
-			createCookie("pageNotification","true",1,false);
-			$('.site-level-notification').css('display', 'none');
+			var siteNotification = this.closest(".site-level-notification");
+	        var siteNotificationDiv = $(siteNotification).find(".cmp-container");
+	        var siteNotificationId = siteNotificationDiv.attr('id');
+			createCookie("pageNotification-"+siteNotificationId,"true",1,false);
+	        $(siteNotification).css('display', 'none');
 		}
 	});
 });
