5f9842df90f9b1ecd4e85a031ff6effc04999d46
MO93 <senthil.kumar.tamilarasan@sunlife.com>
2020-05-05 11:11:15 -0400

    Displaying the date based on locale

--- core/src/main/java/ca/sunlife/web/cms/core/models/ArticleModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/ArticleModel.java
@@ -4,6 +4,7 @@ package ca.sunlife.web.cms.core.models;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
@@ -543,14 +544,26 @@ public class ArticleModel {
   private void setArticlePublishDate(final ValueMap articleContent)
       throws LoginException, RepositoryException {
     String articlePublishedDate = StringUtils.EMPTY;
+    String pageLocaleDefault = StringUtils.EMPTY;
+    
+    try {
+        final String locale = configService.getConfigValues("pageLocale", currentPage.getPath());        
+        if (null != locale && locale.length() > 0) {
+            pageLocaleDefault = locale.split("_") [ 0 ];
+          }
+        
     if (articleContent.containsKey(ARTICLE_PUBLISHED_DATE)) {
       LOGGER.debug("formatting date to {}",
           configService.getConfigValues("articleDateFormat", currentPage.getPath()));
       final SimpleDateFormat formatter = new SimpleDateFormat(
-          configService.getConfigValues("articleDateFormat", currentPage.getPath()));
+          configService.getConfigValues("articleDateFormat", currentPage.getPath()), new Locale(pageLocaleDefault));
       articlePublishedDate = formatter.format( ((GregorianCalendar) articleContent
           .getOrDefault(ARTICLE_PUBLISHED_DATE, new GregorianCalendar())).getTime());
     }
     articleData.put(ARTICLE_PUBLISHED_DATE, articlePublishedDate);
+    }
+    catch(RepositoryException | org.apache.sling.api.resource.LoginException e) {
+  	  LOGGER.error("Error ::ArticleModel :: Article published date :: Exception :: {}", e);
+    }
   }
 }
