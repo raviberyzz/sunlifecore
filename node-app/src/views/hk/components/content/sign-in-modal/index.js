04de3af579931e26ed667f14be8e06604fb5c845
Your Name <you@example.com>
2020-06-04 18:19:46 -0400

    Fix for HK signin

--- node-app/src/views/hk/components/content/sign-in-modal/index.js
+++ node-app/src/views/hk/components/content/sign-in-modal/index.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-    $("a.customer-sign-sm").click(function() {
+    /*$("a.customer-sign-sm").click(function() {
         updateSignInForm('form_signon_mobile');     
       });  
     $('#signin-widget-modal-modal').on('shown.bs.modal', function() {
           updateSignInForm('form_signon');        
-    });
+    });*/
+	//Added for disabling sign call to sunnet
+	$('#signin-widget-modal').unbind("on");
+    $('#signin-widget-modal').unbind("shown.bs.modal");
+    $('#signin-widget-modal').off('show.bs.modal');
     function modalWidth(){
       var winWidth=$(window).width();
       $("#signin-widget-modal").width(winWidth);
