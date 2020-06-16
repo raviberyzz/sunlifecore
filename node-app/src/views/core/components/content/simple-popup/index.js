fc3a1509b32969a57437a30afd9d0b92a325f668
YJ57 <suchana.mitra@sunlife.com>
2020-06-16 16:49:42 +0530

    form popup modification

--- node-app/src/views/core/components/content/simple-popup/index.js
+++ node-app/src/views/core/components/content/simple-popup/index.js
@@ -17,18 +17,3 @@ $(document).ready(function () {
 
 });
 
-//For Lead gen modal dismiss
-
-$("#myModal .modal-body .button").click(function(){
-  if($("#advisor-modal-form")[0]){
-      $("#advisor-modal-form")[0].reset();
-  }
-  else{
-
-  }
-  $("#myModal").modal('toggle');
-
-});
-
-//For Contact Us modal dismiss
-$("#contactusModal .modal-body .button").click(function(){$("#contactusModal").modal('toggle')});
