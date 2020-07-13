011320882f0fdede4bb5911383610a741fe1e1e9
YJ58 <akash.saha@sunlife.com>
2020-07-13 21:46:14 +0530

    os download link

--- node-app/src/views/hk/components/content/moblie-app/index.js
+++ node-app/src/views/hk/components/content/moblie-app/index.js
@@ -477,6 +477,30 @@ $(document).ready(function(){
       if($("#mobile-app-banner")){
         var title='',author='',price='',downloadText='';
         let my_sunlife_app_link='https://play.google.com/store/apps/details?id=com.sunlife.hk.mysunlife';
+        function getOS() {
+          var userAgent = window.navigator.userAgent,
+              platform = window.navigator.platform,
+              macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+              windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+              iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+              os = null;
+        
+          if (macosPlatforms.indexOf(platform) !== -1) {
+            os = 'Mac OS';
+          } else if (iosPlatforms.indexOf(platform) !== -1) {
+            os = 'iOS';
+          } else if (windowsPlatforms.indexOf(platform) !== -1) {
+            os = 'Windows';
+          } else if (/Android/.test(userAgent)) {
+            os = 'Android';
+          } else if (!os && /Linux/.test(platform)) {
+            os = 'Linux';
+          }                
+          if(os=='Mac OS' || os=='iOS' || $('body').hasClass('mac-os-target')){
+            my_sunlife_app_link="https://itunes.apple.com/hk/app/my-sun-life-hk/id1335907850?l=en&mt=8";
+          }
+        }
+        getOS();
         if($('html').attr('lang')=='zh-TW'){
           /* Content for traditional chinese page */
           title='My Sun Life HK 流動應用程式';
