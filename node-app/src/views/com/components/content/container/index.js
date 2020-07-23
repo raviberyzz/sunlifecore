bd78236f84e5042519bc7ee53e35723415f6e3a0
YJ57 <suchana.mitra@sunlife.com>
2020-07-23 19:49:26 +0530

    js change

--- node-app/src/views/com/components/content/container/index.js
+++ node-app/src/views/com/components/content/container/index.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {  
 	//for footer
-	if ( $('#mainfooter .teaser').length) {
-
-	}
-	else {
+	if ( $('#mainfooter .teaser').length ==0 ) {
 		if ($(window).width() < 768) {
 			$('#mainfooter .social-link-icon-wrapper').css({ "padding": "0" });
 		} else {
@@ -15,12 +12,11 @@ $(document).ready(function () {
 			if ($(window).width() < 768) {
 				$('#mainfooter .social-link-icon-wrapper').css({ "padding": "0" });
 			} else {
-				var image_height = $('#mainfooter .teaser').height();
+				var image_height = $('#mainfooter .image').height();
 				$('#mainfooter .social-link-icon-wrapper').css({ "padding": "16px 11px 16px 0" });
 				$('#mainfooter .social-link-icon-wrapper').height(image_height);
 			}
 		});
-	}
-	
 
+	}
 });
