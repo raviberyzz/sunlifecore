ba88b62d70b09204db7a2b1cd8df8b94509140cf
H441 <asmita.mondal@sunlife.com>
2020-07-30 14:41:40 +0530

    banner height same as sign-in

--- node-app/src/views/core/components/content/teaser/index.js
+++ node-app/src/views/core/components/content/teaser/index.js
@@ -10,5 +10,29 @@ $(document).ready(function () {
 		} else {
 			$(".banner-section .right-image-position .hide-in-editorial").insertBefore(".banner-section .right-image-position .right-item");
 		}
+		if ($(window).width() > 767 && $('.same-signin-height').length > 0) {
+			if($('.sign-in').length > 0) {
+				var signInHeight = $('.sign-in').height();
+				$('.same-signin-height').css({
+					"height": signInHeight
+				});
+			}
+		} else {
+            $('.same-signin-height').css({
+					"height": "inherit"
+				});
+        }
 	});
+	if ($(window).width() > 767 && $('.same-signin-height').length > 0) {
+		if($('.sign-in').length > 0) {
+			var signInHeight = $('.sign-in').height();
+			$('.same-signin-height').css({
+                "height": signInHeight
+            });
+		}
+	} else {
+		$('.same-signin-height').css({
+				"height": "inherit"
+			});
+	}
 });
