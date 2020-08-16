f2bad7eab1f0456a546356b481430d2072e1acbe
Dharshak Sagar <dharshak.sagar@sunlife.com>
2020-08-15 20:11:59 -0400

    Babel integration with react

--- node-app/src/views/core/components/content/react-demo/index.js
+++
@@ -1,7 +0,0 @@
-var reactDemo = createReactClass({
-  displayName: "reactDemo",
-  render: function () {
-    return React.createElement(this.props.headingLevel, null, "Hello, ", this.props.headingText);
-  }
-});
-reactComponents["react-demo"] = reactDemo;
