df9773d542f6455f0be82adb36e2c8e38fb8fd1d
yj99 <yj99@INNA012IJV.sunlifecorp.com>
2020-08-21 16:55:12 +0530

    sign changes

--- core/src/main/java/ca/sunlife/web/cms/core/models/SignInModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/SignInModel.java
@@ -4,6 +4,7 @@
 
 package ca.sunlife.web.cms.core.models;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -17,7 +18,6 @@ import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.slf4j.Logger;
@@ -156,7 +156,6 @@ public class SignInModel {
 	/** The hiddenMetadata. */
 	@Inject
 	@Via("resource")
-	@Optional
 	private List<HiddenMetadataModel> hiddenMetadata;
 
 	/**
@@ -170,7 +169,7 @@ public class SignInModel {
 	 * @param signInHeadingText
 	 *            the signInHeadingText to set
 	 */
-	public void setSignInHeadingText(String signInHeadingText) {
+	public void setSignInHeadingText(final String signInHeadingText) {
 		this.signInHeadingText = signInHeadingText;
 	}
 
@@ -185,7 +184,7 @@ public class SignInModel {
 	 * @param mobileAppBadge
 	 *            the mobileAppBadge to set
 	 */
-	public void setMobileAppBadge(String mobileAppBadge) {
+	public void setMobileAppBadge(final String mobileAppBadge) {
 		this.mobileAppBadge = mobileAppBadge;
 	}
 
@@ -200,7 +199,7 @@ public class SignInModel {
 	 * @param mobileAppBadgeLink
 	 *            the mobileAppBadgeLink to set
 	 */
-	public void setMobileAppBadgeLink(String mobileAppBadgeLink) {
+	public void setMobileAppBadgeLink(final String mobileAppBadgeLink) {
 		this.mobileAppBadgeLink = mobileAppBadgeLink;
 	}
 
@@ -215,7 +214,7 @@ public class SignInModel {
 	 * @param accessIDPlaceholder
 	 *            the accessIDPlaceholder to set
 	 */
-	public void setAccessIDPlaceholder(String accessIDPlaceholder) {
+	public void setAccessIDPlaceholder(final String accessIDPlaceholder) {
 		this.accessIDPlaceholder = accessIDPlaceholder;
 	}
 
@@ -230,7 +229,7 @@ public class SignInModel {
 	 * @param passwordPlaceholder
 	 *            the passwordPlaceholder to set
 	 */
-	public void setPasswordPlaceholder(String passwordPlaceholder) {
+	public void setPasswordPlaceholder(final String passwordPlaceholder) {
 		this.passwordPlaceholder = passwordPlaceholder;
 	}
 
@@ -245,7 +244,7 @@ public class SignInModel {
 	 * @param checkboxLabel
 	 *            the checkboxLabel to set
 	 */
-	public void setCheckboxLabel(String checkboxLabel) {
+	public void setCheckboxLabel(final String checkboxLabel) {
 		this.checkboxLabel = checkboxLabel;
 	}
 
@@ -260,7 +259,7 @@ public class SignInModel {
 	 * @param signInButtonLabel
 	 *            the signInButtonLabel to set
 	 */
-	public void setSignInButtonLabel(String signInButtonLabel) {
+	public void setSignInButtonLabel(final String signInButtonLabel) {
 		this.signInButtonLabel = signInButtonLabel;
 	}
 
@@ -275,7 +274,7 @@ public class SignInModel {
 	 * @param formActionUrl
 	 *            the formActionUrl to set
 	 */
-	public void setFormActionUrl(String formActionUrl) {
+	public void setFormActionUrl(final String formActionUrl) {
 		this.formActionUrl = formActionUrl;
 	}
 
@@ -290,7 +289,7 @@ public class SignInModel {
 	 * @param forgotAccessIdLabel
 	 *            the forgotAccessIdLabel to set
 	 */
-	public void setForgotAccessIdLabel(String forgotAccessIdLabel) {
+	public void setForgotAccessIdLabel(final String forgotAccessIdLabel) {
 		this.forgotAccessIdLabel = forgotAccessIdLabel;
 	}
 
@@ -305,7 +304,7 @@ public class SignInModel {
 	 * @param forgotAccessIdLink
 	 *            the forgotAccessIdLink to set
 	 */
-	public void setForgotAccessIdLink(String forgotAccessIdLink) {
+	public void setForgotAccessIdLink(final String forgotAccessIdLink) {
 		this.forgotAccessIdLink = forgotAccessIdLink;
 	}
 
@@ -320,7 +319,7 @@ public class SignInModel {
 	 * @param forgotPasswordLabel
 	 *            the forgotPasswordLabel to set
 	 */
-	public void setForgotPasswordLabel(String forgotPasswordLabel) {
+	public void setForgotPasswordLabel(final String forgotPasswordLabel) {
 		this.forgotPasswordLabel = forgotPasswordLabel;
 	}
 
@@ -335,7 +334,7 @@ public class SignInModel {
 	 * @param forgotPasswordLink
 	 *            the forgotPasswordLink to set
 	 */
-	public void setForgotPasswordLink(String forgotPasswordLink) {
+	public void setForgotPasswordLink(final String forgotPasswordLink) {
 		this.forgotPasswordLink = forgotPasswordLink;
 	}
 
@@ -350,7 +349,7 @@ public class SignInModel {
 	 * @param signInBottomText
 	 *            the signInBottomText to set
 	 */
-	public void setSignInBottomText(String signInBottomText) {
+	public void setSignInBottomText(final String signInBottomText) {
 		this.signInBottomText = signInBottomText;
 	}
 
@@ -365,7 +364,7 @@ public class SignInModel {
 	 * @param domain
 	 *            the domain to set
 	 */
-	public void setDomain(String domain) {
+	public void setDomain(final String domain) {
 		this.domain = domain;
 	}
 
@@ -380,7 +379,7 @@ public class SignInModel {
 	 * @param language
 	 *            the language to set
 	 */
-	public void setLanguage(String language) {
+	public void setLanguage(final String language) {
 		this.language = language;
 	}
 
@@ -395,7 +394,7 @@ public class SignInModel {
 	 * @param logLang
 	 *            the logLang to set
 	 */
-	public void setLogLang(String logLang) {
+	public void setLogLang(final String logLang) {
 		this.logLang = logLang;
 	}
 
@@ -410,7 +409,7 @@ public class SignInModel {
 	 * @param target
 	 *            the target to set
 	 */
-	public void setTarget(String target) {
+	public void setTarget(final String target) {
 		this.target = target;
 	}
 
@@ -425,7 +424,7 @@ public class SignInModel {
 	 * @param errorMsgPlaceholder
 	 *            the errorMsgPlaceholder to set
 	 */
-	public void setErrorMsgPlaceholder(String errorMsgPlaceholder) {
+	public void setErrorMsgPlaceholder(final String errorMsgPlaceholder) {
 		this.errorMsgPlaceholder = errorMsgPlaceholder;
 	}
 
@@ -440,7 +439,7 @@ public class SignInModel {
 	 * @param errorRedirectPath
 	 *            the errorRedirectPath to set
 	 */
-	public void setErrorRedirectPath(String errorRedirectPath) {
+	public void setErrorRedirectPath(final String errorRedirectPath) {
 		this.errorRedirectPath = errorRedirectPath;
 	}
 
@@ -448,15 +447,15 @@ public class SignInModel {
 	 * @return the hiddenMetadata
 	 */
 	public List<HiddenMetadataModel> getHiddenMetadata() {
-		return hiddenMetadata;
+		return Collections.unmodifiableList(hiddenMetadata);
 	}
 
 	/**
 	 * @param hiddenMetadata
 	 *            the hiddenMetadata to set
 	 */
-	public void setHiddenMetadata(List<HiddenMetadataModel> hiddenMetadata) {
-		this.hiddenMetadata = hiddenMetadata;
+	public void setHiddenMetadata(final List<HiddenMetadataModel> hiddenMetadata) {
+		 this.hiddenMetadata = Collections.unmodifiableList(hiddenMetadata);
 	}
 
 	/**
@@ -465,10 +464,8 @@ public class SignInModel {
 	@PostConstruct
 	public void init() {
 		try {
-			LOGGER.debug("SignInModel :: initial value for domain is {}", domain);
 			final String pagePath = currentPage.getPath();
 		    final String domainName = configService.getConfigValues(DOMAIN_STR, pagePath);
-
 			if (null != domain && !StringUtils.isEmpty(domain)) {
 				String domainTargetArray[] = domain.split(Pattern.quote(SEPARATOR));
 				if (domainTargetArray.length > 1) {
@@ -491,8 +488,6 @@ public class SignInModel {
 				LOGGER.debug("SignInModel :: target is {}", target);
 				LOGGER.debug("SignInModel :: errorRedirectPath is {}", errorRedirectPath);
 			}
-		} catch (NullPointerException e) {
-			LOGGER.error("NullPointerException :: init method of SignInModel :: {}", e);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			LOGGER.error("ArrayIndexOutOfBoundsException :: init method of SignInModel :: {}", e);
 		} catch (LoginException e) {
@@ -533,7 +528,7 @@ public class SignInModel {
 		 * @param name
 		 *            the name to set
 		 */
-		public void setName(String name) {
+		public void setName(final String name) {
 			this.name = name;
 		}
 
@@ -548,7 +543,7 @@ public class SignInModel {
 		 * @param value
 		 *            the value to set
 		 */
-		public void setValue(String value) {
+		public void setValue(final String value) {
 			this.value = value;
 		}
 
