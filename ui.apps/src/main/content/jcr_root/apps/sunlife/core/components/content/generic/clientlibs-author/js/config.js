955437b31240fcde1544d1526c8b74670b16b78d
YJ58 <akash.saha@sunlife.com>
2020-08-21 16:47:46 +0530

    grey added

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/generic/clientlibs-author/js/config.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/generic/clientlibs-author/js/config.js
@@ -1,7 +1,7 @@
 var multiPurposeComponentConfig = [
     {
         "componentName": "Stock Ticker Component",
-        "reactComponentName": "react-demo",
+        "reactComponentName": "stock-ticker",
         "props" : [
             {
                 name: "stockTickerType",
@@ -53,19 +53,57 @@ var multiPurposeComponentConfig = [
             }
         ]
     },{
-        "componentName": "Stock Ticker Component 2",
-        "reactComponentName": "StockTickerComponent2",
-        "props" : [
-            {
-                name: "prop1",
-                label: "Heading Text",
-                type: "text"
-            },
-            {
-                name: "prop2",
-                label: "Heading Text 2",
-                type: "text"
-            }
-        ]
-    }
+            "componentName": "Stock Ticker Component",
+            "reactComponentName": "stock-ticker",
+            "props" : [
+                {
+                    name: "stockTickerType",
+                    label: "Stock Ticker Type",
+                    value: "grey",
+                    required: true
+                },
+                {
+                    name: "iconName",
+                    label: "Icon Name",
+                    value: "fa-bar-chart",
+                    required: true
+                },
+                {
+                    name: "stockTickerHeadingText",
+                    label: "",
+                    value: "Stock Ticker Heading Text",
+                    required: true
+                },
+                {
+                    name: "viewAllLink",
+                    label: "View All Link",
+                    value: "/en/investors/share-performance/share-charts/",
+                    required: true
+                },
+                {
+                    name: "viewAllText",
+                    label: "View All Text",
+                    value: "View All",
+                    required: true
+                },
+                {
+                    name: "dataDelayedText",
+                    label: "Data Delayed Text",
+                    value: "Data delayed by 15 minutes",
+                    required: true
+                },
+                {
+                    name: "disclaimerText",
+                    label: "Disclaimer Text",
+                    value: "disclaimer",
+                    required: true
+                },
+                {
+                    name: "disclaimerLink",
+                    label: "Disclaimer Link",
+                    value: "/content/dam/sunlife/legacy/assets/com/Global/files/html/stockDisclaimer.html",
+                    required: true
+                }
+            ]
+        }
 ];
