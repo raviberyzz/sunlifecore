b9fd38828036a021924231848e94bca998f5f323
DE48 <Sri.Harsha@sunlife.com>
2020-09-02 18:55:59 -0400

    pagination code changes

--- node-app/src/views/slgi/components/content/pagination/index.js
+++ node-app/src/views/slgi/components/content/pagination/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     if ($('.pagination')) {
         var getDomain = window.location.hostname;
-        if (getDomain.search("sunnet")) {
+        if (getDomain.indexOf("sunnet") > -1) {
             var paginationLinks = $('.pagination.pagination-list li > a');
             Object.values(paginationLinks).forEach(element => {
                 if (typeof element === 'object' && element.nodeType !== undefined) {
