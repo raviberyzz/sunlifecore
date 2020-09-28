11c614ca3f54fbcfe822f743304b1122dc692954
H441 <asmita.mondal@sunlife.com>
2020-09-28 19:13:42 +0530

    removing authoring specific styling

--- node-app/src/views/core/components/content/full-header/index.js
+++ node-app/src/views/core/components/content/full-header/index.js
@@ -165,12 +165,9 @@ $(document).ready(function () {
     });
 
     // top margin for all pages in mobile & tablet view
-    let wcmMode=getCookie('wcmmode');
-    if( wcmMode != "preview" && wcmMode != "edit") {
         if ($('.full-header').parents('.experiencefragment').next().hasClass('breadcrumb')) {
             $('.breadcrumb').next().addClass('margin-top-fifty');
         } else {
             $('.full-header').parents('.experiencefragment').next().addClass('margin-top-fifty');
         }
-    }
 });
--- node-app/src/views/slgi/components/content/full-header/index.js
+++ node-app/src/views/slgi/components/content/full-header/index.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
     // top margin for all pages in mobile & tablet view
-    let wcmMode=getCookie('wcmmode');
-    if( wcmMode != "preview" && wcmMode != "edit") {
         if ($('.full-header').parents('.recommended-products-article').next().hasClass('breadcrumb')) {
             $('.breadcrumb').next().addClass('margin-top-fifty');
         } else {
             $('.full-header').parents('.recommended-products-article').next().addClass('margin-top-fifty');
         }
-    }
 });
