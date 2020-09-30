5568563528dceeca2c2597e12a8fd37a1d142c09
YJ58 <akash.saha@sunlife.com>
2020-09-30 17:00:31 +0530

    article path

--- node-app/src/views/source/components/content/user-details/index.js
+++ node-app/src/views/source/components/content/user-details/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    if( typeof utag_data != 'undefined' && utag_data != null && typeof userInfo != 'undefined' && userInfo != null ){
-        utag_data['email'] = userInfo.email;
-    }
+    // if( typeof utag_data != 'undefined' && utag_data != null && typeof userInfo != 'undefined' && userInfo != null ){
+    //     utag_data['email'] = userInfo.email;
+    // }
 })
