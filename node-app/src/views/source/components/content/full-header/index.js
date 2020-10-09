3f7d1d901d8d0d53f2d93a345d242a63314323a2
mo92 <krinil.kumar.shantilal.vaja@sunlife.com>
2020-10-09 14:00:05 -0400

    fixed user name

--- node-app/src/views/source/components/content/full-header/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var contextHubData = localStorage.getItem("ContextHubPersistence");
-    if(contextHubData) {
-		var userProfile = JSON.parse(localStorage.getItem("ContextHubPersistence"));
-		var userName = userProfile.store.profile.displayName;
-		$('.utility-nav-links ul[role="navigation"] li:first-child a').text("Welcome" + " " + userName);
-	}
-})
