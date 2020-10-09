62c54bc731576ab068aa48ba4402460ca5eb6ced
mo92 <krinil.kumar.shantilal.vaja@sunlife.com>
2020-10-09 13:41:07 -0400

    user name issue fixed

--- node-app/src/views/source/components/content/user-details/index.js
+++ node-app/src/views/source/components/content/user-details/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-     if( typeof utag_data != 'undefined' && utag_data != null && typeof userInfo != 'undefined' && userProfileHome != null ){
+     if( typeof utag_data != 'undefined' && utag_data != null && typeof userProfileHome != 'undefined' && userProfileHome != null ){
 		 if (typeof ContextHub !== "undefined") {
 			var profileStore   = ContextHub.getStore('profile');
 			var requestUser    = userProfileHome;
