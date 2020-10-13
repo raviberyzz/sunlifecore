3216324be2f761bbc8a9db92e24445984e41f8d3
YJ58 <akash.saha@sunlife.com>
2020-10-13 13:12:20 +0530

    source analytics added

---
+++ node-app/src/views/source/base/source-analytics.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+/* Find people search starts here */
+    $('#find-people-button').click(function(){
+        utag.link({
+            ev_type: 'other',
+            ev_action: 'clk',
+            ev_title: 'find-people-search'
+        });      
+    });
+/* Find people search ends here */
+});
