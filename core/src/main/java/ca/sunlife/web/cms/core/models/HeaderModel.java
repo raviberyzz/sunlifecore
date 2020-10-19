848042397bd89114037b18b100b93ed989bde5af
MO93 <senthil.kumar.tamilarasan@sunlife.com>
2020-10-19 14:26:39 -0400

    Changing core header component for adding different button title for desktop & mobile

--- core/src/main/java/ca/sunlife/web/cms/core/models/HeaderModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/HeaderModel.java
@@ -39,12 +39,36 @@ public interface HeaderModel {
   Resource getMobilenav();
 
   /**
-   * Gets the signin title.
+   * Gets the signin desktop title.
    *
-   * @return the signin title
+   * @return the signin desktop title
    */
   @ Inject
   String getSigninTitle();
+  
+  /**
+   * Gets the signin mobile title.
+   *
+   * @return the signin mobile title
+   */
+  @ Inject
+  String getSigninMobileTitle();
+  
+  /**
+   * Gets the signin button Id.
+   *
+   * @return the signin button Id
+   */
+  @ Inject
+  String getSigninButtonId();
+  
+  /**
+   * Gets the signin mobile title.
+   *
+   * @return the signin mobile title
+   */
+  @ Inject
+  String getSigninAnalyticsId();
   
   /**
    * Gets the signin icon.
