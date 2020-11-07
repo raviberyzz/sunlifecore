b6f4dae8482a91da5a17c7e9e5ca801ba03d18ce
YJ58 <akash.saha@sunlife.com>
2020-11-07 17:51:45 +0530

    source defect

--- node-app/src/views/source/base/source-analytics.js
+++ node-app/src/views/source/base/source-analytics.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
     });
 /* Find people search ends here */
 });
+/* Source accessibility starts here */
+$(document).ready(function(){
+    /* Removing sign-in refrence starts here */
+    if($('.desktop-utility-nav a').attr('href')=='#signinbutton'){
+        $('.desktop-utility-nav a[href="#signinbutton"]').remove();
+    }
+    /* Removing sign-in refrence ends here */
+    });
+/* Source accessibility ends here */
