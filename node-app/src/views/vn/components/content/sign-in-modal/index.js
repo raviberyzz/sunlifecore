bec2687e799280e93e4b3ae3609576bff0f898ac
YJ57 <suchana.mitra@sunlife.com>
2020-11-10 16:03:20 +0530

    vn sign-in button fix, mail function fix

--- node-app/src/views/vn/components/content/sign-in-modal/index.js
+++ node-app/src/views/vn/components/content/sign-in-modal/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $('#signin-widget-modal').unbind("shown.bs.modal");
-    $('#signin-widget-modal').off('show.bs.modal');
+    /*$('#signin-widget-modal').unbind("shown.bs.modal");
+    $('#signin-widget-modal').off('show.bs.modal');*/
     function modalWidth(){
       var winWidth=$(window).width();
       $("#signin-widget-modal").width(winWidth);
