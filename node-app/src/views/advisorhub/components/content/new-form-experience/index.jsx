0998439da01c81e199b08c73085249deb1d199ab
DE48 <Sri.Harsha@sunlife.com>
2020-12-24 09:57:37 -0500

    displaying the dynamic data into the table in new form experience

--- node-app/src/views/advisorhub/components/content/new-form-experience/index.jsx
+++ node-app/src/views/advisorhub/components/content/new-form-experience/index.jsx
@@ -1,12 +1,34 @@
 class NewFormExperience extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-          data : ""
-      }
+        super(props);
+        this.state = {
+            tableData: []
+        }
+        this.getTableData = this.getTableData.bind(this);
     }
-    render(){
-        return(<div>{this.props.text}</div>)
+    componentDidMount() {
+        this.getTableData();
+    }
+    getTableData() {
+        $.ajax({
+            type: "GET",
+            url: "https://cmsdev-auth.ca.sunlife/content/sunlife/external/advisorhub/en/form-page/jcr:content/root/layout_container/container1/generic.forms.en.json",
+            dataType: "json",
+            success: (response) => {
+                this.state.tableData = response;
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        })
+    }
+    render() {
+        return (<div>
+            <table>
+                <thead></thead>
+                <tbody>{this.state.tableData.map(data => { return (<tr>{Object.keys(data).map((key, index) => { return (<td>{data[key]}</td>) })}</tr>) })}</tbody>
+            </table>
+        </div>)
     }
 }
 reactComponents["new-form-experience"] = NewFormExperience;
