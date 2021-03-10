7a2dd4753f6edcf749d424c74a1b469a11c31634
YL21 <aishik.deb@sunlife.com>
2021-03-10 12:01:09 +0530

    remove analytics js

--- node-app/src/views/advisorhub/base/adv-analytics.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var segment = ContextHub.SegmentEngine.getResolvedSegments();
-    var segmentArray = [];
-    for(i=0;i<segment.length;i++){
-    segmentArray.push(segment[i]["title"]);
-    }
-    utag_data['user_segment'] = segmentArray;
-});
