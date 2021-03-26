7b36d07adb65f0020cb2dbfd5dbfcc25bb64c467
YL21 <aishik.deb@sunlife.com>
2021-03-26 11:54:37 +0530

    tool centre js fix

--- node-app/src/views/advisorhub/components/content/tool-centre/index.js
+++ node-app/src/views/advisorhub/components/content/tool-centre/index.js
@@ -1,15 +1,17 @@
-$("select").each(function(){
-    if($(this).attr("data-title").indexOf("AdvisorHub-")==0){
-      $(this).change(function(){
-        var title=$(this).children("option:selected").text();
-        var titleArray = title.split(" ");
-        title="";
-        for(var i=0;i<titleArray.length;i++){
-          title=title+titleArray[i];
-        }
-        title = "AdvisorHub-"+title;
-        $(this).attr("data-title", title);
-        $(this).parents("form").attr("target", title);
-      })
-    }
-  })
+$(document).ready(function(){
+  $("select").each(function(){
+      if($(this).attr("data-title").indexOf("AdvisorHub-")==0){
+        $(this).change(function(){
+          var title=$(this).children("option:selected").text();
+          var titleArray = title.split(" ");
+          title="";
+          for(var i=0;i<titleArray.length;i++){
+            title=title+titleArray[i];
+          }
+          title = "AdvisorHub-"+title;
+          $(this).attr("data-title", title);
+          $(this).parents("form").attr("target", title);
+        })
+      }
+    })
+});
