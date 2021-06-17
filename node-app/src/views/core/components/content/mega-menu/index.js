ab1e64959c13d258a17d84a2302ce00110d7363a
Sudip Chatterjee <yj52@INNA012IJT.sunlifecorp.com>
2021-06-17 15:31:27 +0530

    EBUS-111519

--- node-app/src/views/core/components/content/mega-menu/index.js
+++ node-app/src/views/core/components/content/mega-menu/index.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         } else {
             strLink = strLink.substr(1, (strLink.lastIndexOf('/') - 1));
         }
-        if (pathName.indexOf(strLink) > -1) {
+        if (pathName.indexOf(strLink) > -1 && strLink!="") {
             $(this).addClass("nav-active");
         }
     });
