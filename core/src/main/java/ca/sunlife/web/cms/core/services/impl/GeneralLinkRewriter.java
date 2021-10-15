be17dc9302ad02919ff0af8dd920f7c8bb777d8b
cs66 <victor.wang@sunlife.com>
2021-10-15 15:49:24 -0400

    change Externalizer to Sun Life SiteConfigService
    remove the custom domains from externalizer configs since it's no longer
    needed

--- core/src/main/java/ca/sunlife/web/cms/core/services/impl/GeneralLinkRewriter.java
+++ core/src/main/java/ca/sunlife/web/cms/core/services/impl/GeneralLinkRewriter.java
@@ -1,11 +1,10 @@
 package ca.sunlife.web.cms.core.services.impl;
 
 import com.adobe.cq.xf.ExperienceFragmentLinkRewriterProvider;
-
 import com.adobe.cq.xf.ExperienceFragmentVariation;
-import com.day.cq.commons.Externalizer;
 
-import org.apache.sling.api.resource.ResourceResolverFactory;
+import ca.sunlife.web.cms.core.services.SiteConfigService;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
@@ -13,13 +12,9 @@ import org.slf4j.LoggerFactory;
 
 @Component(service = ExperienceFragmentLinkRewriterProvider.class, immediate = true)
 public class GeneralLinkRewriter implements ExperienceFragmentLinkRewriterProvider {
-
-	/** The Externalizer. */
-	@Reference
-	private Externalizer externalizer;
 	
-    @Reference
-    private ResourceResolverFactory resourceResolverFactory;
+	@ Reference
+	private SiteConfigService siteConfigService;
 
 	/** The logger. */
 	private final static Logger logger = LoggerFactory.getLogger(GeneralLinkRewriter.class);
@@ -30,12 +25,13 @@ public class GeneralLinkRewriter implements ExperienceFragmentLinkRewriterProvid
 	public String rewriteLink(String link, String tag, String attribute) {
 		logger.debug("entering rewriteLink...");
 		logger.debug("original link --> "+link);
-		if (externalizer == null) {
+		if (siteConfigService == null) {
 			// if there was an error, then we do not modify the link
-			logger.debug("externalizer is null, link is not transformed.");
+			logger.debug("siteConfigService is null, link is not transformed.");
 			return null;
 		}
-		link = transformedLink(link, externalizer);
+		link = link.replaceAll(".html", "");
+		link = siteConfigService.getPageUrl(link);
 		logger.debug("transformedLink --> "+link);
 		return link;
 	}
@@ -54,42 +50,6 @@ public class GeneralLinkRewriter implements ExperienceFragmentLinkRewriterProvid
 	@Override
 	public int getPriority() {
 		return 1;
-	}
-
-	private String transformedLink(String input, Externalizer externalizer) {
-		logger.debug("entering transformedLink...");
-		if (input.contains("/content/sunlife/")) {
-			logger.debug("input is --> "+input);
-			input = format(input);
-			input = externalizer.externalLink(resourceResolverFactory.getThreadResourceResolver(), domain, input);
-			logger.debug("output is --> "+input);
-		}
-		return input;
-	}
-
-	private static String format(String input) {
-		String value = "";
-		Boolean lastIndex = false;
-		if (input.endsWith("/")) {
-			lastIndex = true;
-		}
-		if (input.contains("/")) {
-			String[] tokens = input.split("/");
-			if (tokens.length > 4) {
-				for (int i = 1; i < tokens.length; i++) {
-					if (i > 4) {
-						value += "/" + tokens[i];
-					}
-				}
-			}
-		} else {
-			value = input;
-		}
-		value = value.replaceAll(".html", "/");
-		if (lastIndex) {
-			value = value + "/";
-		}
-		return value;
 	}
 
 }
