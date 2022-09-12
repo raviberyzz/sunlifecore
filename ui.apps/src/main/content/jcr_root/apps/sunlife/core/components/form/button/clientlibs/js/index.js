19094504d5be4df476b493a177e777d905f474cc
Al62 <Sreek.Vangimalla@sunlife.com>
2022-09-12 10:13:47 -0400

    Removing role=button

---
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/form/button/clientlibs/js/index.js
@@ -0,0 +1,7 @@
+//Adding role=button
+/*$('span').each(function () {
+	var checkspan = ($("span").hasClass("button-class"));
+	if(checkspan) {
+        $("span.button-class").parent().attr("role","button");
+    }
+});*/
