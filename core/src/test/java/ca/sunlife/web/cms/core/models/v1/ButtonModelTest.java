9f9e490109647370b11b85da0cda4927e225223d
ML\qy95 <raho.mohamed@sunlife.com>
2023-10-19 08:43:50 -0400

    MP-548 - Button Component

---
+++ core/src/test/java/ca/sunlife/web/cms/core/models/v1/ButtonModelTest.java
@@ -0,0 +1,47 @@
+package ca.sunlife.web.cms.core.models.v1;
+
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(AemContextExtension.class)
+public class ButtonModelTest {
+
+	@InjectMocks
+    private ButtonModel button;
+
+    @Mock
+    ButtonModel mockButton;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+ @Test
+	void testInitWithText() {
+		button.getText();
+		button.getLink();
+		button.getTarget();
+		button.getIcon();
+		button.isIconPosition();
+		button.getButtonType();
+		button.getDataTitle();
+		button.getAccessibilityLabel();
+		button.getSpacing();
+		when(mockButton.isIconPosition()).thenReturn(true);
+		when(mockButton.getButtonType()).thenReturn("btn-primary");
+	}
+
+}
+
+
+
+
