fe050ff0e300ff0495cf5f91343ca606b8d740cf
Uma Maheshwaran <uma.maheshvaran@sunlife.com>
2023-10-25 15:03:25 -0400

    Junit fix for core version upgrade

--- core/src/test/java/ca/sunlife/web/cms/core/models/LeftNavItemImplTest.java
+++ core/src/test/java/ca/sunlife/web/cms/core/models/LeftNavItemImplTest.java
@@ -4,6 +4,7 @@
 package ca.sunlife.web.cms.core.models;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -19,46 +20,62 @@ import com.adobe.cq.wcm.core.components.internal.models.v2.PageImpl;
 import com.adobe.cq.wcm.core.components.models.NavigationItem;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
+import com.adobe.cq.wcm.core.components.commons.link.Link;
+import com.adobe.cq.wcm.core.components.commons.link.LinkBuilder;
+import com.adobe.cq.wcm.core.components.commons.link.LinkManager;
+import com.day.cq.wcm.api.components.Component;
 
 /**
- * @author mo92
- * The class LeftNavItemImplTest.
+ * @author mo92 The class LeftNavItemImplTest.
  */
 public class LeftNavItemImplTest {
 
-	@ Mock
+	@Mock
 	private Page page;
-	
+
 	private LeftNavItemImpl itemImpl;
-	
-	@ Mock
+
+	@Mock
 	private SlingHttpServletRequest request;
-	
-	@ Mock
-	private List <NavigationItem> navigationItems;
-	
-	@ Mock
+
+	@Mock
+	private List<NavigationItem> navigationItems;
+
+	@Mock
 	private PageManager pageManager;
-	
-	@ Mock
+
+	@Mock
+	private LinkManager linkManager;
+
+	@Mock
+	private Component component;
+
+	@Mock
 	private ValueMap valueMap;
-	
-	@ BeforeEach
-	void setUp( ) {
+
+	@Mock
+	private LinkBuilder linkBuilder;
+
+	@BeforeEach
+	void setUp() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
-	@ Test
+
+	@Test
 	void testLeftNavItemImpl() {
 		when(page.getPageManager()).thenReturn(pageManager);
 		when(page.getProperties()).thenReturn(valueMap);
 		when(valueMap.get(PageImpl.PN_REDIRECT_TARGET, String.class)).thenReturn("Titlee");
 		when(pageManager.getPage("Titlee")).thenReturn(page);
 		when(page.getPath()).thenReturn("homepage");
-	//	itemImpl = new LeftNavItemImpl(page, true, request, 1, navigationItems, "nav");
-	//	assertNotNull(itemImpl.getPath());
+		Link<?> link = mock(Link.class);
+		LinkBuilder linkBuilder = mock(LinkBuilder.class);
+		when(linkManager.get(page)).thenReturn(linkBuilder);
+		when(linkManager.get(page).build()).thenReturn(link);
+		itemImpl = new LeftNavItemImpl(page, true, request, 1, navigationItems, "nav", linkManager, component);
+		assertNotNull(itemImpl.getPath());
 	}
-	
+
 	@Test
 	void testgetTitle() {
 		when(page.getNavigationTitle()).thenReturn("title1");
@@ -70,7 +87,12 @@ public class LeftNavItemImplTest {
 		when(valueMap.get(PageImpl.PN_REDIRECT_TARGET, String.class)).thenReturn("Titlee");
 		when(pageManager.getPage("Titlee")).thenReturn(page);
 		when(page.getPath()).thenReturn("homepage");
-	//	itemImpl = new LeftNavItemImpl(page, true, request, 1, navigationItems, "nav");
-	//	itemImpl.getTitle();
+		Link<?> link = mock(Link.class);
+		LinkBuilder linkBuilder = mock(LinkBuilder.class);
+		when(linkManager.get(page)).thenReturn(linkBuilder);
+		when(linkManager.get(page).build()).thenReturn(link);
+		itemImpl = new LeftNavItemImpl(page, true, request, 1, navigationItems, "nav", linkManager, component);
+		itemImpl.getTitle();
+		assertNotNull(itemImpl.getPath());
 	}
 }
