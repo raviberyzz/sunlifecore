649b64bc8c99fb438f9069181797375182bd2ea9
ML\yb97 <mayank.goyal@sunlife.com>
2023-11-16 22:41:49 +0530

    Adding Sticky Bar Component and its configuration

---
+++ core/src/main/java/ca/sunlife/web/cms/core/models/StickyBarModel.java
@@ -0,0 +1,131 @@
+/*
+ *
+ */
+
+package ca.sunlife.web.cms.core.models;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+/**
+ * The Interface StickyBarModel.
+ *
+ * @author ASIA
+ * @version 1.0
+ */
+@ Model (adaptables = {
+    Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public interface StickyBarModel {
+
+  /**
+   * Gets the links.
+   *
+   * @return the links
+   */
+  @ Inject
+  List <Links> getLinks();
+
+  /**
+   * The Interface Links.
+   *
+   * @author ASIA
+   * @version 1.0
+   */
+  @ Model (adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+  interface Links {
+
+    /**
+     * Gets the link name.
+     *
+     * @return the link name
+     */
+    @ Inject
+    String getLinkName();
+
+    /**
+     * Gets the link url.
+     *
+     * @return the link url
+     */
+    @ Inject
+    String getLinkUrl();
+
+    /**
+     * Gets the target.
+     *
+     * @return the target
+     */
+    @ Inject
+    String getLinkTarget();
+    
+    /**
+     * Gets the dataTitle.
+     *
+     * @return the dataTitle
+     */
+    @ Inject
+    String getDataTitle();
+  }
+
+  /**
+   * Gets the additionalButton.
+   *
+   * @return the additionalButton
+   */
+  @ Inject
+  String getAdditionalButton();
+
+  /**
+   * Gets the CTA Label.
+   *
+   * @return the CTA Label
+   */
+  @ Inject
+  String getButtonLabel();
+
+  /**
+   * Gets the CTA Link.
+   *
+   * @return the CTA Link
+   */
+  @ Inject
+  String getButtonLink();
+
+  /**
+   * Gets the CTA Target.
+   *
+   * @return the CTA Target
+   */
+  @ Inject
+  String getButtonTarget();
+
+  /**
+   * Gets the Second CTA Label.
+   *
+   * @return the Second CTA Label
+   */
+  @ Inject
+  String getSecButtonLabel();
+
+  /**
+   * Gets the Second CTA Link.
+   *
+   * @return the Second CTA Link
+   */
+  @ Inject
+  String getSecButtonLink();
+
+  /**
+   * Gets the Second CTA Target.
+   *
+   * @return the Second CTA Target
+   */
+  @ Inject
+  String getSecButtonTarget();
+
+}
---
+++ ui.frontend/src/main/webpack/components/content/sticky-bar/index.js
@@ -0,0 +1,4 @@
+$("#stickyBarLinks li a").click(function () {
+    $("#stickyBarLinks li a").removeClass("active");
+    $(this).addClass("active");
+});
