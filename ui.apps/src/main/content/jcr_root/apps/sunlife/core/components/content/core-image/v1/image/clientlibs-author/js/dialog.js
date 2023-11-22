c4db4944f376ae6dfe0efa01985b1e270b206be6
ML\np20 <praveen.poonia@sunlife.com>
2023-11-22 16:03:11 -0500

    Image component updated

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-image/v1/image/clientlibs-author/js/dialog.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-image/v1/image/clientlibs-author/js/dialog.js
@@ -1,31 +1,42 @@
-(function($,$document){
+(function($, $document){
     "use strict"
 	var dialogContentSelector = ".cmp-image__editor";
-    var $imagetypeContainerGroup;	
-	var isAvatar;	
-    $(document).on("dialog-loaded", function(e) {
-       var $dialog        = e.dialog;
+	var isAvatar;
+
+    $document.on("dialog-loaded", dialogOnloadHandler);
+    $document.on("change", dialogContentSelector + ' coral-checkbox[name="./isAvatar"]', avatarCheckHandler);
+
+    function dialogOnloadHandler(e) {
+        var $dialog        = e.dialog;
         var $dialogContent = $dialog.find(dialogContentSelector);
         var dialogContent  = $dialogContent.length > 0 ? $dialogContent[0] : undefined;
+        
         if (dialogContent) {
-			isAvatar = dialogContent.querySelector('coral-checkbox[name="./isAvatar"]');	
-			$imagetypeContainerGroup = $dialogContent.find(".cmp-image__editor-image-type-container");
-			toggleImagetypeContainerGroup(isAvatar);
+			isAvatar = dialogContent.querySelector('coral-checkbox[name="./isAvatar"]');
+            toggleAvatarOptions(isAvatar.checked);
 		}
-    });
-	
-	$(document).on("change", dialogContentSelector + ' coral-checkbox[name="./isAvatar"]', function(e) {
-        toggleImagetypeContainerGroup(e.target);
-    });
-	
-	function toggleImagetypeContainerGroup(checkbox) {
-        if (checkbox) {
-            if (checkbox.checked) {                
-                $imagetypeContainerGroup.show();
-            } else {
-                $imagetypeContainerGroup.hide();                
-            }           
+    }
+
+    function toggleAvatarOptions(isChecked) {
+        console.log('isChecked', isChecked)
+        var $imagetypeContainerGroup = $('.cmp-image__editor').find(".cmp-image__editor-image-type-container");
+        var $mobileImage = $('.cmp-image__editor').find('[name="./fileMobile"]').parent();
+        var $desktopImage = $('.cmp-image__editor').find('[name="./file"]');
+        var $desktopImgLabel = $desktopImage.parent().find(".coral-Form-fieldlabel");
+        $imagetypeContainerGroup.toggle(isChecked);
+        $mobileImage.toggle(!isChecked);
+        if(isChecked) {
+            $desktopImage.removeAttr("data-cq-fileupload-required");
+            $desktopImgLabel.text('Avatar Image');
+        } else {
+            $desktopImage.attr("data-cq-fileupload-required");
+            $desktopImgLabel.text('Desktop Image*');
         }
     }
 	
-})($,$(document));
+	function avatarCheckHandler(e) {
+        var checkbox = e.target;
+        toggleAvatarOptions(checkbox.checked);
+    }
+	
+})($, $(document));
