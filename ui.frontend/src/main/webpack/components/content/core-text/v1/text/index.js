7bcec3a08dcf94668da0d61b181ece164a1b0839
Malini Sankar <tz93@clarica.com>
2023-11-27 11:26:55 -0500

    MP-1342 Popover Tab Order Issue Fixed

--- ui.frontend/src/main/webpack/components/content/core-text/v1/text/index.js
+++ ui.frontend/src/main/webpack/components/content/core-text/v1/text/index.js
@@ -11,7 +11,7 @@ const TextComp = {
 		TextComp.onDOMready();
 
 		var popoverTriggerList = [].slice.call(document.querySelectorAll('.cmp-text [data-bs-toggle="popover"'))
-			var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+		var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 			return new bootstrap.Popover(popoverTriggerEl)
 		})
 		TextComp.initPopover();
@@ -21,28 +21,52 @@ const TextComp = {
 		$(CONST.SELECTOR.internalLink).append('<i class="far fa-arrow-circle-right"></i>');
 
 		$(CONST.SELECTOR.pdfLink).prepend('<i class="far fa-file-pdf"></i>');
-		
+
 		$('.cmp-text .popover-button').html('<span class="visually-hidden">Informational Popup</span><i class="fal fa-info-circle" role="presentation"></i>');
 
-		$(document).on("click", ".popover .btn-close-popover" , function(){
-			let popoverElem = $(this).closest(".popover");
+		function getPopoverId(element) {
+			let popoverElem = $(element).closest(".popover");
 			let id = popoverElem.attr('id');
+			return [popoverElem, id];
+		}
+
+		$(document).on("click", ".popover .btn-close-popover", function () {
+			let [popoverElem, id] = getPopoverId($(this));
 			popoverElem.hide();
-			$('[aria-describedby='+id+']').click().focus();
+			$('[aria-describedby=' + id + ']').click().focus();
 		});
-		$(document).on("keydown", '.popover .btn-close-popover', function(event){
+		$(document).on("keydown", '.popover .btn-close-popover', function (event) {
 			if (event.key === 'Escape') {
 				event.preventDefault();
-				let popoverElem = $(this).closest(".popover");
-				let id = popoverElem.attr('id');
+				let [popoverElem, id] = getPopoverId($(this));
 				popoverElem.hide();
-				$('[aria-describedby='+id+']').click().focus();
+				$('[aria-describedby=' + id + ']').click().focus();
+			}
+			else if (event.key == 'Tab' && event.shiftKey) {
+				event.preventDefault();
+				let [popoverElem, id] = getPopoverId($(this));
+				$('[aria-describedby=' + id + ']').focus();
+			}
+			else if (event.key == 'Tab') {
+				let [popoverElem, id] = getPopoverId($(this));
+				let selectableElements = [].slice.call(document.querySelectorAll('select, input, textarea, button, a'));
+				selectableElements.find((value, index) => {
+					if(value.getAttribute("aria-describedby") === id){
+						event.preventDefault();
+						selectableElements[index + 1].focus();
+					}
+				})
 			}
 		});
-		$(document).on("keydown", '.popover-button' , function(event){
+		$(document).on("keydown", '.popover-button', function (event) {
+			let popOverId = $(this).attr('aria-describedby');
 			if (event.key == 'Escape') {
 				event.preventDefault();
 				$(this).click().focus();
+			}
+			else if (event.key == 'Tab' && popOverId !== undefined && !event.shiftKey) {
+				event.preventDefault();
+				$('#' + popOverId + ' .btn-close-popover').focus();
 			}
 		});
 	},
@@ -57,7 +81,7 @@ const TextComp = {
 		});
 
 		function getCustomClass(popoverTriggerElem){
-			return getDataAttrContent(popoverTriggerElem, 'bs-title') ? 'sl-popover' : 'sl-popover sl-popover-headless' 
+			return getDataAttrContent(popoverTriggerElem, 'bs-title') ? 'sl-popover' : 'sl-popover sl-popover-headless'
 		}
 
 		function getPopoverConfig(popoverTriggerEl){
