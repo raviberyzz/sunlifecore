e84fb28810ea2618458270826c8dc654d2c16bc8
ML\tu85 <kiran.vedantam@sunlife.com>
2023-11-30 14:51:16 -0500

    Review comments

--- core/src/main/java/ca/sunlife/web/cms/core/models/v1/MegaMenuModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/MegaMenuModel.java
@@ -3,15 +3,16 @@ package ca.sunlife.web.cms.core.models.v1;
 import com.adobe.acs.commons.models.injectors.annotation.ChildResourceFromRequest;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import java.util.List;
 
-
 @Model(adaptables = {SlingHttpServletRequest.class},
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
         resourceType = MegaMenuModel.RESOURCE_TYPE)
+@Exporter(name = "jackson", extensions = "json")
 public interface MegaMenuModel {
 
     public final static String RESOURCE_TYPE = "sunlife/core/components/content/core-megamenu/v1/megamenu";
