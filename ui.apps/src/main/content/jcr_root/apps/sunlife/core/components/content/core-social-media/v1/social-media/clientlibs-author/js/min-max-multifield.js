b2d993a5c071cfcbc4517ca2600e058379eb61af
VF52 <deepak.chauhan@sunlife.com>
2023-12-11 19:44:09 +0530

    Adding custom validation to the dialog

---
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-social-media/v1/social-media/clientlibs-author/js/min-max-multifield.js
@@ -0,0 +1,29 @@
+(function(window, document, $, Granite, undefined) {
+
+    $(window).adaptTo("foundation-registry").register("foundation.validation.validator", {
+
+        selector: "[data-foundation-validation^='multifield-social-max']",
+        validate: function(el) {
+
+            var socialMediaType = $('[name="./type"]').val();
+            if (socialMediaType === 'shareoptions') {
+                var validationName = el.getAttribute("data-validation");
+                var res = validationName.split(",");
+                var min;
+                var max;
+
+                max = res[0].replace("multifield-social-max-", "");
+                min = res[1].replace("multifield-social-min-", "");
+
+                max = parseInt(max);
+                if (el.items.length < min) {
+                    return "Min needed items is " + min
+                }
+                if (el.items.length > max) {
+                    return "Max allowed items is " + max
+                }
+            }
+        }
+    });
+
+})(window, document, Granite.$, Granite);
