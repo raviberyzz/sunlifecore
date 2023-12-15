1e0bdde509626d14a577843851fa08544740724c
Malini Sankar <tz93@clarica.com>
2023-12-15 08:48:52 -0500

    desktop to mobile view function updated

--- ui.frontend/src/main/webpack/components/content/core-header/v1/header/index.js
+++ ui.frontend/src/main/webpack/components/content/core-header/v1/header/index.js
@@ -2,20 +2,22 @@
 
     const classLists = document.getElementsByClassName("sl-utility-bar")[0];
 
-    window.addEventListener("resize", function () {
-    
+    function offCanvas () {
         if(window.innerWidth <= 1239) {   
             classLists.classList.remove("sl-utility-bar");
             classLists.classList.add("sl-utility-bar-offcanvas");
-           var node =  document.getElementById('offcanvas-body');
-                   node.append(classLists);
+            var node =  document.getElementById('offcanvas-body');
+            node.append(classLists);
         }
      
         else{
             classLists.classList.remove("sl-utility-bar-offcanvas");
             classLists.classList.add("sl-utility-bar");
-            var node1 =  document.getElementById('nav-header');
-            node1.insertAdjacentElement("beforebegin", classLists);
+            var node =  document.getElementById('nav-header');
+            node.insertAdjacentElement("beforebegin", classLists);
         }
-    }); 
+    }
+
+    window.addEventListener("load", offCanvas); 
+    window.addEventListener("resize", offCanvas); 
 })()
