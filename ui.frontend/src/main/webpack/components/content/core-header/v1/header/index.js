5911b89ad70d52b294782e8bca6fe220d8059bda
Malini Sankar <tz93@clarica.com>
2023-12-19 09:59:42 -0500

    js updated for hamburger menu view

--- ui.frontend/src/main/webpack/components/content/core-header/v1/header/index.js
+++ ui.frontend/src/main/webpack/components/content/core-header/v1/header/index.js
@@ -1,23 +1,37 @@
 (function () {
 
-    const classLists = document.getElementsByClassName("sl-utility-bar")[0];
+    const utilityLists = document.getElementsByClassName("sl-utility-bar")[0];
+    const navLists = document.getElementsByClassName("sl-header-offcanvas")[0];
 
-    function offCanvas () {
-        if(window.innerWidth <= 1239) {   
-            classLists.classList.remove("sl-utility-bar");
-            classLists.classList.add("sl-utility-bar-offcanvas");
-            var node =  document.getElementById('offcanvas-body');
-            node.append(classLists);
+    function offCanvas() {
+        if (window.innerWidth <= 1239) {
+            utilityLists.classList.remove("sl-utility-bar");
+            utilityLists.classList.add("sl-utility-bar-offcanvas");
+            var bodyNode = document.getElementById('offcanvas-body');
+            bodyNode.append(utilityLists);
         }
-     
-        else{
-            classLists.classList.remove("sl-utility-bar-offcanvas");
-            classLists.classList.add("sl-utility-bar");
-            var node =  document.getElementById('nav-header');
-            node.insertAdjacentElement("beforebegin", classLists);
+
+        else {
+            utilityLists.classList.remove("sl-utility-bar-offcanvas");
+            utilityLists.classList.add("sl-utility-bar");
+            var navNode = document.getElementById('nav-header');
+            navNode.insertAdjacentElement("beforebegin", utilityLists);
         }
     }
 
-    window.addEventListener("load", offCanvas); 
-    window.addEventListener("resize", offCanvas); 
+    function callMenuOpen() {
+        var headerNode = document.getElementById('nav-header');
+        headerNode.insertAdjacentElement("afterend", navLists);
+    }
+
+    function callMenuClose() {
+        var slnavNode = document.getElementById('sl-nav');
+        slnavNode.append(navLists);
+    }
+
+    window.addEventListener("load", offCanvas);
+    window.addEventListener("resize", offCanvas);
+
+    document.getElementById("menuOpen").onclick = callMenuOpen;
+    document.getElementById("menuClose").onclick = callMenuClose;
 })()
