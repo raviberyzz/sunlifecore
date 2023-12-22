18c95e836b81560f5795f28f91b8349ac6daf64a
Malini Sankar <tz93@clarica.com>
2023-12-22 15:23:11 -0500

    Feedback updated

--- ui.frontend/src/main/webpack/components/content/core-header/v1/header/index.js
+++ ui.frontend/src/main/webpack/components/content/core-header/v1/header/index.js
@@ -1,8 +1,10 @@
 (function () {
-
+       
+    /* Retrieves the element based on a classname*/
     const utilityLists = document.getElementsByClassName("sl-utility-bar")[0];
     const navLists = document.getElementsByClassName("sl-header-offcanvas")[0];
 
+    /* Handles the desktop and mobile view*/
     function offCanvas() {
         if (window.innerWidth <= 1239) {
             utilityLists.classList.remove("sl-utility-bar");
@@ -19,11 +21,12 @@
         }
     }
 
+    /* Handles hamburger menu open clicks*/
     function callMenuOpen() {
         var headerNode = document.getElementById('nav-header');
         headerNode.insertAdjacentElement("afterend", navLists);
     }
-
+    /* Handles hamburger menu close clicks*/
     function callMenuClose() {
         var slnavNode = document.getElementById('sl-nav');
         slnavNode.append(navLists);
