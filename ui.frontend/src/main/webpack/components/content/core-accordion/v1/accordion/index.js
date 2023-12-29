21467262ef53f1dd91371485ad553f3e2f367bcf
OD22 <Sravani.Bammidi@sunlife.com>
2023-12-29 00:18:26 -0500

    animation changes

--- ui.frontend/src/main/webpack/components/content/core-accordion/v1/accordion/index.js
+++ ui.frontend/src/main/webpack/components/content/core-accordion/v1/accordion/index.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
     $(".sl-accordion .accordion-header").keydown(function(e) {
         if(e.key === "Escape" && $(this).siblings(".accordion-collapse").hasClass("expanded")) {                
-            $(this).find(".sl-icon").removeClass("hide").removeClass("show");
-            $(this).find(".accordion-button").attr("aria-expanded", false);
-            $(this)
-                .siblings(".accordion-collapse")
-                .removeClass("expanded")
-                .addClass("collapse");
-            $(this).find(".fa-chevron-up").addClass("hide");
-            $(this).find(".fa-chevron-down").addClass("show");
+          $(this).find(".sl-icon").removeClass("hide").removeClass('show');
+          $(this).find(".accordion-button").attr("aria-expanded", false);
+          $(this).siblings(".accordion-collapse").removeClass("expanded")
+          $(this).find(".fa-chevron-up").addClass("hide");
+          $(this).find(".fa-chevron-down").addClass("show");
+          $(this).siblings(".accordion-collapse").css('height', "");
         }
     });
   $(".sl-accordion .accordion-header").click(function () {
@@ -16,28 +14,23 @@ $(document).ready(function () {
     if ($(this).siblings(".accordion-collapse").hasClass("expanded")) {
       $(this).find(".sl-icon").removeClass("hide").removeClass('show');
       $(this).find(".accordion-button").attr("aria-expanded", false);
-      $(this)
-        .siblings(".accordion-collapse")
-        .removeClass("expanded")
-        .addClass("collapse");
+      $(this).siblings(".accordion-collapse").removeClass("expanded")
       $(this).find(".fa-chevron-up").addClass("hide");
       $(this).find(".fa-chevron-down").addClass("show");
-      $(this).siblings(".accordion-collapse").height(0);
+      $(this).siblings(".accordion-collapse").css('height', "");
     } else {
       if (singleExpansion == "true") {
-        $(".sl-accordion .accordion-collapse")
-          .removeClass("expanded")
-          .addClass("collapse"); // closes all open accordians
-        $(".sl-accordion .accordion-button").attr("aria-expanded", false);
-        $(".sl-icon").removeClass("show").removeClass("hide");
-        $(".fa-chevron-down").addClass("show");
-        $(".fa-chevron-up").addClass("hide");
+        $(this).parents('.accordion-item').find(".sl-accordion .accordion-collapse").removeClass("expanded") // closes all open accordians
+        $(this).parents('.accordion-item').find(".accordion-collapse").css('height', "");
+        $(this).parents('.accordion-item').find(".accordion-button").attr("aria-expanded", false);
+        $(this).parents('.accordion-item').find(".sl-icon").removeClass("show").removeClass("hide");
+        $(this).parents('.accordion-item').find(".fa-chevron-down").addClass("show");
+        $(this).parents('.accordion-item').find(".fa-chevron-up").addClass("hide");
       }
       $(this).find(".sl-icon").removeClass("hide").removeClass('show');
       $(this)
         .siblings(".accordion-collapse")
-        .removeClass("collapse")
-        .addClass("expanded"); // opens clicked accordian
+        .addClass("show expanded"); // opens clicked accordian
       $(this).find(".accordion-button").attr("aria-expanded", true);
       $(this).find(".fa-chevron-up").addClass("show");
       $(this).find(".fa-chevron-down").addClass("hide");
@@ -54,7 +47,7 @@ $(document).ready(function () {
   if ($(".sl-accordion").hasClass("first-open")) {
     if (accLength > 1) {
       var firstAcc = acc[0];
-      $(firstAcc).find(".accordion-collapse").addClass("expanded");
+      $(firstAcc).find(".accordion-collapse").addClass("show expanded");
       $(firstAcc).find(".accordion-button").attr("aria-expanded", true);
       $(this).find(".fa-chevron-up").addClass("show").removeClass("hide");
       $(this).find(".fa-chevron-down").addClass("hide").removeClass("show");
