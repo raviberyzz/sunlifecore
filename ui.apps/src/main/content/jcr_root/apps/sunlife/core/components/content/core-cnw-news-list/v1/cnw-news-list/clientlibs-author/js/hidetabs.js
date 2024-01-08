ca76bab7dbe06305c8ee04fd0be0ee578011a848
ML\tu85 <kiran.vedantam@sunlife.com>
2024-01-08 16:01:28 -0500

    Newslist changes

---
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-cnw-news-list/v1/cnw-news-list/clientlibs-author/js/hidetabs.js
@@ -0,0 +1,40 @@
+(function($, $document) {
+
+    "use strict";
+
+    $(document).on("foundation-contentloaded", function(e) {
+
+        $("#tab-hide-show").each(function() {
+            showHide();
+        });
+
+    });
+
+
+    $document.on("change", ".newsType", function(e) {
+        showHide();
+    });
+
+$(document).on("dialog-ready", function() {
+
+});
+
+
+
+    function showHide() {
+        var list = $("#tab-hide-show").find("coral-tab");
+        let overview = $(".overview").prop("selected");
+        let listing = $(".listing").prop("selected")
+
+        if (overview) {
+            list[1].removeAttribute("hidden");
+        } else {
+            list[1].setAttribute("hidden", true)
+        }
+        if (listing) {
+            list[2].removeAttribute("hidden");
+        } else {
+            list[2].setAttribute("hidden", true);
+        }
+    }
+})($, $(document));
