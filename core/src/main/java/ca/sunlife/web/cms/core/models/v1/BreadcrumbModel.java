03056490ab86fa7ef91ece36dd692769007c4d22
ML\qy95 <raho.mohamed@sunlife.com>
2024-01-08 15:56:24 -0500

    remove data section from dialog

--- core/src/main/java/ca/sunlife/web/cms/core/models/v1/BreadcrumbModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/BreadcrumbModel.java
@@ -30,8 +30,5 @@ import com.adobe.cq.wcm.core.components.models.Breadcrumb;
     private String accessibilityLabel;
 
     @ValueMapValue
-    private String dataTitle;
-
-    @ValueMapValue
     private String spacing;
 }
