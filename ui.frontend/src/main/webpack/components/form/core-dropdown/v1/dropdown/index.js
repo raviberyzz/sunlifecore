25ab81520fe27a996360c8df03963a6ef3a4be7f
Kateryna Semenova <ad01@clarica.com>
2024-01-17 13:53:53 -0500

    Display label correctly if the instruction text is blank; Add functionality to handle Blur events, to close the dropdown MP-671

--- ui.frontend/src/main/webpack/components/form/core-dropdown/v1/dropdown/index.js
+++ ui.frontend/src/main/webpack/components/form/core-dropdown/v1/dropdown/index.js
@@ -1,199 +1,89 @@
 /**
 * Form Dropdown Component specific JS
 */
- 
-(function () {
- 
-    /**
-    * Bind event on module.
-    */
-   function bindEvent(){
-      // Bind click event on dropdown.
-      $('form .sl-dropdown').on('click keyup', '.combo-input', dropdownComboHandler);
-      $('form .sl-dropdown').on('click keyup', '.combo-option', dropDownOptionHandler);
-   }
-   
-    /**
-    * Handles dropdown combo clicks.
-    * @param {Event} e - The event object.
-    */
-    function dropdownComboHandler(e) {
-        console.log(e);
-        console.log(e.type);
-   
-        if (e.keyCode == '40') {
-            //arrow down
-            e.preventDefault();
-            let currentComboInput = e.currentTarget;
-   
-            $(currentComboInput).closest('.combo').addClass("open");
-            currentComboInput.setAttribute("aria-expanded", "true");
-            raiseLabel(currentComboInput);
-            let currentOptionSelected = $(currentComboInput).next().find(".option-current")
-            console.log(currentOptionSelected);
-            console.log(currentOptionSelected.next());
-   
-            if ( currentOptionSelected.length && currentOptionSelected.next() ) {
-              console.log('next');
-              $(currentOptionSelected).toggleClass("option-current");
-              $(currentOptionSelected).next().toggleClass("option-current");
-            } else {
-                console.log('First');
-               $(currentComboInput).next().children(":first").toggleClass("option-current");
-            }
-   
-        } else if (e.type == 'click') {
-            e.preventDefault();
-            let currentComboInput = e.currentTarget;
-            $(currentComboInput).closest('.combo').toggleClass("open");
-            currentComboInput.setAttribute("aria-expanded", "true");
-            raiseLabel(currentComboInput);
-        }
-    }
-   
-   
-    /**
-    * Handles dropdown option clicks.
-    * @param {Event} e - The event object.
-    */
-    function dropDownOptionHandler(e) {
-      console.log(e);
-      console.log(e.type)
-   
-      e.preventDefault();
-      let currentElement = e.currentTarget;
-      let currentDropdown = $(currentElement);
-      let linkText = $(currentElement).text();
-      raiseLabel(currentDropdown);
-      appendSelectedText(currentDropdown, linkText);
-      selectOption(currentDropdown);
-      $(currentDropdown).closest('.combo').toggleClass("open");
-    }
-   
-   
-    /**
-    * Retrieves the inner text of an element based on a selector.
-    * @param {HTMLElement} elem - The HTML element.
-    * @param {string} selector - The CSS selector.
-    * @returns {string | undefined} - The inner text of the element, or undefined if not found.
-    */
-    function getInnerText(elem, selector) {
-      if ($(elem).length && $(elem).find(selector).length) {
-        return $(elem).find(selector)[0].innerText;
-      }
-    }
-   
-    /**
-    * Adds 'raised' class to the dropdown input label.
-    * @param {HTMLElement} dropDown - The dropdown element.
-    */
-    function raiseLabel(dropDown) {
-        $(dropDown).find('label').addClass('raised active');
-    }
-   
-    /**
-    * Appends text to the dropdown input selected element.
-    * @param {HTMLElement} dropDown - The dropdown element.
-    * @param {string} text - The text to append.
-    */
-    function appendSelectedText(dropDown, text) {
-        $(dropDown[0]).closest('.combo').find('div.combo-input-selected').text(text)
-    }
-   
-    /**
-    * Selects an option within the dropdown menu.
-    * @param {HTMLElement} optionElem - The option element.
-    */
-    function selectOption(optionElem) {
-      $(optionElem).closest('.combo-menu').find('.option-selected').removeClass('option-selected');
-      $(optionElem).addClass('option-selected');
-    }
-   
-    /**
-    * Initialize the module.
-    */
-    function init() {
-      bindEvent();
-    }
-   
-    init();
-   
-  })()
 
-  /**
-* Form Dropdown Component specific JS
-*/
- 
 (function () {
- 
+
   /**
   * Bind event on module.
   */
- function bindEvent(){
-    // Bind click event on dropdown.
-    $('form .sl-dropdown').on('click keyup', '.combo-input', dropdownComboHandler);
-    $('form .sl-dropdown').on('click keyup', '.combo-option', dropDownOptionHandler);
- }
- 
+  function bindEvent() {
+    // Bind mousedown event on dropdown.
+    $('form .sl-dropdown').on('mousedown keyup', '.combo-input', dropdownComboHandler);
+    $('form .sl-dropdown').on('mousedown keyup', '.combo-option', dropDownOptionHandler);
+    $('form .sl-dropdown').on('blur', '.combo-input', dropDownOnBlur);
+  }
+
+  /**
+  * Blur event on dropdown.
+  */
+  function dropDownOnBlur(e) {
+    e.preventDefault();
+    let currentComboInput = e.currentTarget;
+    $(currentComboInput).closest('.combo').removeClass("open");
+    console.log('dropDownOnBlur');
+  }
+
   /**
   * Handles dropdown combo clicks.
   * @param {Event} e - The event object.
   */
   function dropdownComboHandler(e) {
-      console.log(e);
-      console.log(e.type);
- 
-      if (e.keyCode == '40') {
-          //arrow down
-          e.preventDefault();
-          let currentComboInput = e.currentTarget;
- 
-          $(currentComboInput).closest('.combo').addClass("open");
-          currentComboInput.setAttribute("aria-expanded", "true");
-          raiseLabel(currentComboInput);
-          let currentOptionSelected = $(currentComboInput).next().find(".option-current");
-          console.log(currentOptionSelected);
-          console.log(currentOptionSelected.next());
- 
-          if ( currentOptionSelected.length && currentOptionSelected.next() ) {
-            console.log('next');
-            $(currentOptionSelected).toggleClass("option-current");
-            $(currentOptionSelected).next().toggleClass("option-current");
-          } else {
-              console.log('First');
-             $(currentComboInput).next().children(":first").toggleClass("option-current");
-          }
- 
-      } else if (e.type == 'click') {
-          
-          e.preventDefault();
-          let currentComboInput = e.currentTarget;
-          $(currentComboInput).closest('.combo').toggleClass("open");
-          currentComboInput.setAttribute("aria-expanded", "true");
-          raiseLabel(currentComboInput);
+    console.log('dropdownComboHandler');
+    //console.log(e);
+    //console.log(e.type);
+
+    if (e.keyCode == '40') {
+      //arrow down
+      e.preventDefault();
+      let currentComboInput = e.currentTarget;
+
+      $(currentComboInput).closest('.combo').addClass("open");
+      currentComboInput.setAttribute("aria-expanded", "true");
+      raiseLabel(currentComboInput);
+      let currentOptionSelected = $(currentComboInput).next().find(".option-current")
+      //console.log(currentOptionSelected);
+      //console.log(currentOptionSelected.next());
+
+      if (currentOptionSelected.length && currentOptionSelected.next()) {
+        //console.log('next');
+        $(currentOptionSelected).toggleClass("option-current");
+        $(currentOptionSelected).next().toggleClass("option-current");
+      } else {
+        //console.log('First');
+        $(currentComboInput).next().children(":first").toggleClass("option-current");
       }
+
+    } else if (e.type == 'mousedown') {
+      e.preventDefault();
+      let currentComboInput = e.currentTarget;
+      $(currentComboInput).closest('.combo').toggleClass("open");
+      currentComboInput.setAttribute("aria-expanded", "true");
+      raiseLabel(currentComboInput);
+    }
   }
- 
- 
+
   /**
   * Handles dropdown option clicks.
   * @param {Event} e - The event object.
   */
   function dropDownOptionHandler(e) {
-    console.log(e);
-    console.log(e.type)
- 
+    console.log('dropDownOptionHandler');
+    //console.log(e);
+    //console.log(e.type)
+
     e.preventDefault();
     let currentElement = e.currentTarget;
-    let currentDropdown = $(currentElement);
+    let currentDropdownElement = $(currentElement);
     let linkText = $(currentElement).text();
-    raiseLabel(currentDropdown);
-    appendSelectedText(currentDropdown, linkText);
-    selectOption(currentDropdown);
-    $(currentDropdown).closest('.combo').toggleClass("open");
+    //console.log('linkText: ' + linkText);
+
+    appendSelectedText(currentDropdownElement, linkText);
+    selectOption(currentDropdownElement);
+    raiseLabel(currentDropdownElement.parent().parent().find('.combo-input'));
+    $(currentDropdownElement).closest('.combo').toggleClass("open");
   }
- 
- 
+
   /**
   * Retrieves the inner text of an element based on a selector.
   * @param {HTMLElement} elem - The HTML element.
@@ -205,24 +95,31 @@
       return $(elem).find(selector)[0].innerText;
     }
   }
- 
+
   /**
-  * Adds 'raised' class to the dropdown input label.
+  * Adds 'raised active' classes to the dropdown input label.
   * @param {HTMLElement} dropDown - The dropdown element.
   */
   function raiseLabel(dropDown) {
-      $(dropDown).find('label').addClass('raised active');
+    console.log('raiseLabel');
+    if ($('.combo').find('.combo-input-selected')[0].innerText != "") {
+      if (!(document.getElementById('default-selected-label').matches(".raised.active"))) {
+        //console.log('raised');
+        $(dropDown).find('label').addClass('raised active');
+        $(dropDown).find('.combo-input-selected').removeClass("d-none");
+      }
+    }
   }
- 
+
   /**
   * Appends text to the dropdown input selected element.
   * @param {HTMLElement} dropDown - The dropdown element.
   * @param {string} text - The text to append.
   */
   function appendSelectedText(dropDown, text) {
-      $(dropDown[0]).closest('.combo').find('div.combo-input-selected').text(text)
+    $(dropDown[0]).closest('.combo').find('div.combo-input-selected').text(text)
   }
- 
+
   /**
   * Selects an option within the dropdown menu.
   * @param {HTMLElement} optionElem - The option element.
@@ -231,14 +128,14 @@
     $(optionElem).closest('.combo-menu').find('.option-selected').removeClass('option-selected');
     $(optionElem).addClass('option-selected');
   }
- 
+
   /**
   * Initialize the module.
   */
   function init() {
     bindEvent();
   }
- 
+
   init();
- 
+
 })()
