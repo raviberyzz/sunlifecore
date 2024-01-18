efa8e04dd9e9cfaa7db67a24e7fbd12f3dea4fe8
ML\tu85 <kiran.vedantam@sunlife.com>
2024-01-18 13:52:26 -0500

    Bug fix MP-1902, MP-1912

--- core/src/main/java/ca/sunlife/web/cms/core/models/v1/MenuItems.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/MenuItems.java
@@ -1,11 +1,11 @@
 package ca.sunlife.web.cms.core.models.v1;
 
-import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = {SlingHttpServletRequest.class},
+@Model(adaptables = {Resource.class},
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public interface MenuItems {
     /**
