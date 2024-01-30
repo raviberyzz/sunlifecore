d304a7e945be20a090824d26092bd6955ea5ff26
ML\tu85 <kiran.vedantam@sunlife.com>
2024-01-30 10:49:28 -0500

    MP-2109

--- core/src/main/java/ca/sunlife/web/cms/core/models/v1/AccordionModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/AccordionModel.java
@@ -1,49 +1,22 @@
 package ca.sunlife.web.cms.core.models.v1;
 
-import org.apache.sling.api.SlingHttpServletRequest;
+import com.adobe.cq.wcm.core.components.models.Accordion;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import com.adobe.cq.wcm.core.components.models.Accordion;
-import com.adobe.cq.wcm.core.components.internal.models.v1.AccordionImpl;
+public interface AccordionModel extends Accordion {
+    String getHeadingElement();
 
-import lombok.Getter;
- 
-@Getter
-@ Model (adaptables = { SlingHttpServletRequest.class,
-    Resource.class }, adapters = Accordion.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, resourceType = AccordionModel.RESOURCE_TYPE)
-public class AccordionModel extends AccordionImpl implements Accordion {
-	
-	protected static final String RESOURCE_TYPE = "sunlife/core/components/content/core-accordion/v1/accordion";	
-	
-	@ScriptVariable
-	private ResourceResolver resolver;
-		
-	@ValueMapValue 
-	private String headingElement; 	 
-	 
-	@ValueMapValue
-	private String accessibilityLabel;	 
-	 
-	@ValueMapValue
-	private String dataTitle;	 
-	 
-	@ValueMapValue
-	private String spacing; 
-	 
-	@ValueMapValue
-	private String singleExpansion;	 
-	 
-	@ValueMapValue
-	private String[] expandedItems;	 		
-	
-	public AccordionModel() {
-	    super();
-	}
-	
+    String getAccessibilityLabel();
 
+    String getDataTitle();
+
+    String getSpacing();
+
+    String getSingleExpansion();
+
+    String[] getExpandedItems();
 }
--- core/src/test/java/ca/sunlife/web/cms/core/models/v1/AccordionModelTest.java
+++ core/src/test/java/ca/sunlife/web/cms/core/models/v1/AccordionModelTest.java
@@ -5,9 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension; 
@@ -15,23 +13,23 @@ import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 @ExtendWith(AemContextExtension.class)
 public class AccordionModelTest {
 
-	private final AemContext context = new AemContext();	
-	AccordionModel accordion;
+	private final AemContext context = new AemContext();
+	AccordionModelImpl accordion;
 	
 	@Mock
 	private ModelFactory modelFactory;
 	
 	@BeforeEach
-    public void setUp(AemContext context) throws Exception{    	 	
-    	context.addModelsForClasses(AccordionModel.class);
-    	context.load().json("/ca/sunlife/web/cms/core/models/v1/accordion/accordion.json", "/content");
+    public void setUp(AemContext context) throws Exception{
+		context.addModelsForClasses(AccordionModelImpl.class);
+		context.load().json("/ca/sunlife/web/cms/core/models/v1/accordion/accordion.json", "/content");
     }
 
 	@Test
-	void testGetterMethods() {		
+	void testGetterMethods() {
 		context.currentResource("/content/accordion");
-    	accordion = context.request().adaptTo(AccordionModel.class);
-    	assertEquals("h3", accordion.getHeadingElement());
+		accordion = context.request().adaptTo(AccordionModelImpl.class);
+		assertEquals("h3", accordion.getHeadingElement());
     	assertEquals("Sample accessibility label", accordion.getAccessibilityLabel());
     	assertEquals("Sample data title", accordion.getDataTitle());
     	assertEquals("true", accordion.getSingleExpansion());
