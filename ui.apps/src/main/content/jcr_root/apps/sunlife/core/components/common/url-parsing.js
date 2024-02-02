de2de441b3e9710be5f4b2828f6526a293911284
ML\vb30 <amit.chauhan@sunlife.com>
2024-02-02 18:39:22 +0530

    MP-2108 : code updated based on the review comments

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/common/url-parsing.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/common/url-parsing.js
@@ -1,20 +1,16 @@
 "use strict";
+const HTML_EXTENSION = ".html";
 use(function() {
     var navPath = this.path;
-    if (navPath == null || navPath.equals("")) {
+    if (navPath == null || navPath === "") {
         return {
             navPath: navPath
         };
     }
     if (navPath.includes("/content/sunlife")) {
-        if (navPath.includes("/?")) {
-            navPath = navPath.replace("/?", ".html?");
-        } else if (navPath.includes("/#")) {
-            navPath = navPath.replace("/#", ".html#");
-        } else if (navPath.includes("#")) {
-            navPath = navPath.replace("#", ".html#");
-        } else {
-            navPath = navPath + ".html";
+        navPath = navPath.replace(/([\/#?])$/, HTML_EXTENSION + "$1");
+        if (!navPath.includes(HTML_EXTENSION)) {
+            navPath += HTML_EXTENSION;
         }
     }
     return {
