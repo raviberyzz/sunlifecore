150845e569dd968fd940e6861d6adac5c8faf12a
OD22 <Sravani.Bammidi@sunlife.com>
2024-02-05 21:47:33 -0500

    1st commit for form component 2

---
+++ ui.frontend/src/main/webpack/components/form/core-text/v1/text/index.js
@@ -0,0 +1,41 @@
+(function () {
+    //function use for field validation on blur
+    function fieldValidationBlurEventHandler() {
+        let $field = $(this);
+        if($field.val() === ''){
+            $field.parents(".form-floating").addClass("sl-input-error");
+        }
+    }
+    //function use for field validation on keyup
+    function fieldValidationKeyEventHandler() {
+        const $field = $(this);
+        const $fieldContainer = $field.parents(".form-floating");
+        if($field.val() === ''){
+            $fieldContainer.addClass("sl-input-error");
+        }
+        else {
+            $fieldContainer.removeClass("sl-input-error");
+        }
+        if($fieldContainer.hasClass('sl-text-area')){
+            $fieldContainer.find(".character-count .count").html($field.val().length)
+        }
+    }
+    //function use for field validation on key press
+    function fieldValidationKeyPressEventHandler() {
+        const $field = $(this);
+        const $fieldContainer = $field.parents(".form-floating");
+        if($fieldContainer.hasClass('sl-text-area')){
+            $fieldContainer.find(".character-count .count").html($field.val().length+1)
+        }
+    }
+
+    
+    function init() {
+        let $field = $(".sl-text-field .form-control.required, .sl-text-area .form-control.required");
+        $field.keyup(fieldValidationKeyEventHandler);
+        $field.blur(fieldValidationBlurEventHandler);
+        $field.keypress(fieldValidationKeyPressEventHandler);
+    }
+    init();
+
+})()
