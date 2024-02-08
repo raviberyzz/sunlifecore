35d3f2c269dbab2e2c48294254b64380a17bbf6b
ML\qy95 <raho.mohamed@sunlife.com>
2024-02-08 08:20:33 -0500

    Bugs fixed:
    MP-2193 - Alignment of Button is off- ( Site notification component)
    
    MP-2194 - Bottom spacing is not being applied at the bottom- ( Site notification component)
    
    MP-2206 - No option to add aria label to Button - (Site notification component)

--- core/src/main/java/ca/sunlife/web/cms/core/models/v1/impl/NotificationImpl.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/impl/NotificationImpl.java
@@ -6,13 +6,18 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import lombok.Getter;
 
+import ca.sunlife.web.cms.core.constants.NotificationConstants;
 import ca.sunlife.web.cms.core.models.v1.Notification;
 
-@Model(adaptables = { SlingHttpServletRequest.class }, adapters = { Notification.class }, resourceType = {
+@Model(adaptables = { Resource.class }, adapters = { Notification.class }, resourceType = {
         NotificationImpl.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class NotificationImpl implements Notification {
 
@@ -37,6 +42,9 @@ public class NotificationImpl implements Notification {
     private String buttonTarget;
 
     @ValueMapValue
+    private String buttonAccessibilityLabel;
+
+    @ValueMapValue
     private String accessibilityLabel;
 
     @ValueMapValue
@@ -49,30 +57,20 @@ public class NotificationImpl implements Notification {
    @Getter
    private String iconType;
 
-
-    protected static final String INFO_ICON = "fa-info sl-icon_color_information";
-
-    protected static final String SUCCESS_ICON = "fa-check-circle-yes sl-icon_color_success";
-
-    protected static final String WARNING_ICON = "fa-exclamation-circle sl-icon_color_warning";
-   
-    protected static final String ERROR_ICON = "fa-exclamation-triangle sl-icon_color_error";
-
-
     @PostConstruct
     private void initModel() {
         if(notificationType != null){
 			if (notificationType.equals("info") ) {
-				this.iconType = INFO_ICON;
+				this.iconType = NotificationConstants.INFO_ICON;
 			}    
 			if (notificationType.equals("success")) {
-				this.iconType = SUCCESS_ICON;
+				this.iconType = NotificationConstants.SUCCESS_ICON;
 			}			
 			if (notificationType.equals("warning")) {
-				this.iconType = WARNING_ICON;
+				this.iconType = NotificationConstants.WARNING_ICON;
 			}			
 			if (notificationType.equals("danger")) {
-			  this.iconType = ERROR_ICON;
+			  this.iconType = NotificationConstants.ERROR_ICON;
 			}  
 		}
     }
@@ -105,6 +103,11 @@ public class NotificationImpl implements Notification {
     @Override
     public String getButtonTarget() {
         return buttonTarget;
+    }
+
+    @Override
+    public String getButtonAccessibilityLabel() {
+        return buttonAccessibilityLabel;
     }
 
     @Override
