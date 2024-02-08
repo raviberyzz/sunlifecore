b56f13b42794375d1d15c3906d49a415aa2db086
ML\qy95 <raho.mohamed@sunlife.com>
2024-02-08 08:26:56 -0500

    re-added impl changes

--- core/src/test/java/ca/sunlife/web/cms/core/models/v1/impl/NotificationImplTest.java
+++ core/src/test/java/ca/sunlife/web/cms/core/models/v1/impl/NotificationImplTest.java
@@ -1,36 +1,133 @@
 package ca.sunlife.web.cms.core.models.v1.impl;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import javax.annotation.PostConstruct;
 
-import io.wcm.testing.mock.aem.junit5.AemContext;
-import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import lombok.Getter;
 
-@ExtendWith({ AemContextExtension.class })
-public class NotificationImplTest {
+import ca.sunlife.web.cms.core.models.v1.Notification;
 
-    private final AemContext context = new AemContext();
-    NotificationImpl notification;
+@Model(adaptables = { SlingHttpServletRequest.class }, adapters = { Notification.class }, resourceType = {
+        NotificationImpl.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class NotificationImpl implements Notification {
 
-    @BeforeEach
-    public void setUp(AemContext context) throws Exception{
-        context.addModelsForClasses(NotificationImpl.class);
-        context.load().json("/ca/sunlife/web/cms/core/models/v1/notification/notification.json", "/content");
+    protected static final String RESOURCE_TYPE = "sunlife/core/components/content/core-notification/v1/notification";
+
+    @ValueMapValue
+    private String notificationType;
+
+    @ValueMapValue
+    private String notificationHeading;
+
+    @ValueMapValue
+    private String text;
+
+    @ValueMapValue
+    private String buttonTitle;
+
+    @ValueMapValue
+    private String buttonURL;
+
+    @ValueMapValue
+    private String buttonTarget;
+
+    @ValueMapValue
+    private String buttonAccessibilityLabel;
+
+    @ValueMapValue
+    private String accessibilityLabel;
+
+    @ValueMapValue
+    private String dataTitle;
+
+    @ValueMapValue
+    private String spacing;
+
+
+   @Getter
+   private String iconType;
+
+
+    protected static final String INFO_ICON = "fa-info sl-icon_color_information";
+
+    protected static final String SUCCESS_ICON = "fa-check-circle-yes sl-icon_color_success";
+
+    protected static final String WARNING_ICON = "fa-exclamation-circle sl-icon_color_warning";
+   
+    protected static final String ERROR_ICON = "fa-exclamation-triangle sl-icon_color_error";
+
+
+    @PostConstruct
+    private void initModel() {
+        if(notificationType != null){
+			if (notificationType.equals("info") ) {
+				this.iconType = INFO_ICON;
+			}    
+			if (notificationType.equals("success")) {
+				this.iconType = SUCCESS_ICON;
+			}			
+			if (notificationType.equals("warning")) {
+				this.iconType = WARNING_ICON;
+			}			
+			if (notificationType.equals("danger")) {
+			  this.iconType = ERROR_ICON;
+			}  
+		}
     }
 
-    @Test
-	void testGetterMethods() {		
-		context.currentResource("/content/notification");
-    	notification = context.request().adaptTo(NotificationImpl.class);
-    	assertEquals("warning", notification.getNotificationType());
-        assertEquals("Sample notification heading", notification.getNotificationHeading());
-        assertEquals("Sample button title", notification.getButtonTitle());
-        assertEquals("/content/sunlife/external/ca/en/explore-products/insurance", notification.getButtonURL());
-    	assertEquals("Sample accessibility label", notification.getAccessibilityLabel());
-    	assertEquals("Sample data title", notification.getDataTitle());
-    	assertEquals("mb-sl12", notification.getSpacing());    	
-	}
+    @Override
+    public String getNotificationType() {
+        return notificationType;
+    }
+
+    @Override
+    public String getNotificationHeading() {
+        return notificationHeading;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String getButtonTitle() {
+        return buttonTitle;
+    }
+
+    @Override
+    public String getButtonURL() {
+        return buttonURL;
+    }
+
+    @Override
+    public String getButtonTarget() {
+        return buttonTarget;
+    }
+
+    @Override
+    public String getButtonAccessibilityLabel() {
+        return buttonAccessibilityLabel;
+    }
+
+
+    @Override
+    public String getAccessibilityLabel() {
+        return accessibilityLabel;
+    }
+
+    @Override
+    public String getDataTitle() {
+        return dataTitle;
+    }
+
+    @Override
+    public String getSpacing() {
+        return spacing;
+    }
 }
