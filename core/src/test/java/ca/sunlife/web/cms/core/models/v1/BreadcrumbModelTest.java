294b010f028225f58cac70ef75406f6a8f9525df
ML\vb30 <amit.chauhan@sunlife.com>
2024-02-24 18:30:22 +0530

    Junit Updated

--- core/src/test/java/ca/sunlife/web/cms/core/models/v1/BreadcrumbModelTest.java
+++ core/src/test/java/ca/sunlife/web/cms/core/models/v1/BreadcrumbModelTest.java
@@ -1,6 +1,5 @@
 package ca.sunlife.web.cms.core.models.v1;
 
-import ca.sunlife.web.cms.core.models.v1.impl.FooterImpl;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,33 +10,36 @@ import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@ExtendWith({AemContextExtension.class})
+public class BreadcrumbModelTest {
 
-@ExtendWith(AemContextExtension.class)
-public class SitemapModelTest {
-    private  final Logger LOGGER = LoggerFactory.getLogger(SitemapModelTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BreadcrumbModelTest.class);
     private final AemContext context = new AemContext();
+
     @InjectMocks
-    private SitemapModel sitemapMode1;
+    private BreadcrumbModel breadcrumbMode1;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        context.addModelsForClasses(SitemapModel.class);
-        context.load().json("/ca/sunlife/web/cms/core/models/v1/sitemap/sitemap.json", "/content");
+        context.addModelsForClasses(BreadcrumbModel.class);
+        context.load().json("/ca/sunlife/web/cms/core/models/v1/breadcrumb/breadcrumb.json", "/content");
     }
 
     @Test
     public void testInit() {
         try {
-            context.currentResource("/content/sitemap");
-            SitemapModel sitemapModel = context.request().adaptTo(SitemapModel.class);
-            sitemapMode1.getSpacing();
-            sitemapMode1.getAccessibilityLabel();
+            context.currentResource("/content/breadcrumb");
+            BreadcrumbModel breadcrumbModel = context.request().adaptTo(BreadcrumbModel.class);
+            breadcrumbMode1.getSpacing();
+            breadcrumbMode1.getLangcode();
+            breadcrumbMode1.getStartLevel();
+
+
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
         }
     }
-
 }
 
 
