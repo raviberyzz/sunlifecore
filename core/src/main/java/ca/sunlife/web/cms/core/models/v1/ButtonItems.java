6df2c4df9e949c5edba641b08b806e452d61ea99
VF52 <deepak.chauhan@sunlife.com>
2024-02-26 22:47:48 +0530

    Adding missing file

---
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/ButtonItems.java
@@ -0,0 +1,26 @@
+package ca.sunlife.web.cms.core.models.v1;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import lombok.Getter;
+
+@Getter
+@Model(adaptables = { SlingHttpServletRequest.class, Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class ButtonItems {
+
+	@ValueMapValue
+	String buttonType;
+
+	@ValueMapValue
+	String buttonText;
+
+	@ValueMapValue
+	String linkURL;
+
+	@ValueMapValue
+	String linkTarget;
+}
