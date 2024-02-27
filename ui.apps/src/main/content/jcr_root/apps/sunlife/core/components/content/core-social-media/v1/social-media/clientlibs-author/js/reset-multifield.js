1da024bcce99a4c77cc8aa4f60573b1f0bc29af1
Uma Maheshwaran <uma.maheshvaran@sunlife.com>
2024-02-27 16:41:50 -0500

    MP-747:Update to Social media to hide social links specific for article card inclusion

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-social-media/v1/social-media/clientlibs-author/js/reset-multifield.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-social-media/v1/social-media/clientlibs-author/js/reset-multifield.js
@@ -1,7 +1,7 @@
 (function(document, $) {
     "use strict";
     $(document).on("foundation-contentloaded", function(e) {
-        showHideHandler($(".cq-dialog-dropdown-showhide", e.target));
+        initializeSocialShare(e);
     });
 
     $(document).on("change", ".cq-dialog-dropdown-showhide", function(e) {
@@ -9,9 +9,9 @@
     });
 
     function showHideHandler(el) {
-		
+        
         var selectedValue = el.val();
-		
+        
         if (selectedValue == 'sociallinks') {
             var targetElement = el.closest('div[class^="coral-Form-fieldwrapper"]').siblings('[data-showhidetargetvalue="shareoptions"]');
             var $targetElement = $(targetElement);
@@ -27,4 +27,20 @@
             });
         }
     }
+
+    function initializeSocialShare(e) {
+        const currentComponentPath =  Granite.author.DialogFrame.currentDialog.editable.path;
+        const parentPath = currentComponentPath.substr(0, currentComponentPath.lastIndexOf("/"));
+        const articleType = CQ.shared.HTTP.get(parentPath + '/sling%3AresourceType').responseText;
+        const componentType = articleType.substr(articleType.lastIndexOf("/"));
+        
+        if(componentType == '/article') {
+            $('.social-share-dialog').find('coral-tab-label:contains("Spacing")').parent().attr("hidden", true);
+            $('coral-select[name="./type"]').find('coral-select-item[value="shareoptions"]').attr("selected", true);
+            $('.social-share-dialog').find('label:contains("Select Type")').parent().attr("hidden", true);
+        } 
+        showHideHandler($('coral-select[name="./type"]'));
+        
+    }
+
 })(document, Granite.$);
