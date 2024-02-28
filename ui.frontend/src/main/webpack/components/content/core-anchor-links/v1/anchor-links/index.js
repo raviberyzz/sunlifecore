a3297daefe28df38f0d5dabb2e7a77d430cbe10a
OD22 <Sravani.Bammidi@sunlife.com>
2024-02-27 21:19:57 -0500

    updated the code for PR comments

--- ui.frontend/src/main/webpack/components/content/core-anchor-links/v1/anchor-links/index.js
+++ ui.frontend/src/main/webpack/components/content/core-anchor-links/v1/anchor-links/index.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
-  function getAllHeadingForAnchorLinks() {
-    const startLevel = parseInt($(".anchor-link-lists").attr("data-startLevel").replace("h",""));
-    const stopLevel = parseInt($(".anchor-link-lists").attr("data-stopLevel").replace("h",""));
-    let $anchorLinks = [];
-    for(let i = startLevel; i <= stopLevel; i++) {
-      $anchorLinks.push("h"+i);
-    }
-    return $anchorLinks;
-  }
+  
   //Function used to update the active anchor link on click event
   function handleClickEventForActiveLink() {
     $(".sl-anchor-links li").removeClass("active-anchor");
@@ -15,19 +7,15 @@ $(document).ready(function () {
   }
   //Function used to generate the anchorlinks
   function loadAnchorLinks() {
-    let listIndex = 0;
-    getAllHeadingForAnchorLinks().map((item) => {
-      const $anchorLinks = $(item)
-      if($anchorLinks.length === 0) return;
-      const $anchorLinksList = $(".sl-anchor-links ul");
-      let anchorLinksListItem = '';
-      $anchorLinks.each(function(index, item){
-        $(this).attr("id", "anchorLinkHeading-"+listIndex);
-        anchorLinksListItem += '<li class=""><a class="anchorLinkHeading-'+listIndex+'" href="#anchorLinkHeading-'+listIndex+'">'+$(this).html()+'</a></li>';
-        listIndex++;
-      })  
-      $anchorLinksList.append(anchorLinksListItem);    
+    const $anchorLinks = $("h2");
+    if($anchorLinks.length === 0) return;
+    const $anchorLinksList = $(".sl-anchor-links ul");
+    let anchorLinksListItem = '';
+    $anchorLinks.each(function(index, item){
+      $(this).attr("id", "anchorLinkHeading-"+index);
+      anchorLinksListItem += '<li class=""><a class="anchorLinkHeading-'+index+'" href="#anchorLinkHeading-'+index+'">'+$(this).html()+'</a></li>';
     })
+    $anchorLinksList.html(anchorLinksListItem);
   }
   var handleScroll = function handleScroll(e) {
     let linkIds = [];
@@ -40,7 +28,7 @@ $(document).ready(function () {
       var targetElement = document.getElementById(linkId);
       if (targetElement) {
         var eleRect = targetElement === null || targetElement === void 0 ? void 0 : targetElement.getBoundingClientRect();
-        var isInView = eleRect.top >= 0 && $(".anchorLinkHeading-"+(index + 1))?.top <= window.innerHeight;
+        var isInView = eleRect.top >= 0 && eleRect.bottom <= window.innerHeight;
         if (isInView) {
           $(".sl-anchor-links li").removeClass("active-anchor");
           $(".anchorLinkHeading-"+index).parents("li").addClass("active-anchor");
