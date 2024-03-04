b7a8af9a6da74b92a4819ff207d1dda22b0b546d
VF52 <deepak.chauhan@sunlife.com>
2024-03-04 23:46:56 +0530

    Hiding StartLevel and StopLevel from dialog and handling it from front end

--- core/src/main/java/ca/sunlife/web/cms/core/models/v1/AnchorLinksModel.java
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/AnchorLinksModel.java
@@ -2,6 +2,7 @@ package ca.sunlife.web.cms.core.models.v1;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
@@ -23,8 +24,5 @@ public class AnchorLinksModel extends TableOfContentsImpl{
    
     @ValueMapValue
     String anchorHeading;
-
-    @ValueMapValue
-    String spacing;
-
+    
 }
