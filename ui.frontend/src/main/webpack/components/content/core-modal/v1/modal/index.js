ba94b670ea9e0b596ff6e46ff3e7459370ab61bb
Kateryna Semenova <ad01@clarica.com>
2024-03-04 11:56:11 -0500

    Add styles to display modal in Edit mode, and add trigger on modal functionality

--- ui.frontend/src/main/webpack/components/content/core-modal/v1/modal/index.js
+++ ui.frontend/src/main/webpack/components/content/core-modal/v1/modal/index.js
@@ -1,3 +1,21 @@
-// $(document).ready(function () {
-   
-// }); 
+/**
+* Display a 'triggerOnScroll' modal when scrolling halfway down the page.
+* Display the modal only once per browser session, managed by reading a cookie.
+*/
+$(document).ready(function () {
+    $(window).scroll(function () {
+        let modalTriggerOnScroll = document.querySelectorAll('[data-triggerOnScroll]')[0];
+        if (modalTriggerOnScroll != undefined)
+        var modalId = modalTriggerOnScroll.getAttribute('id');
+        if ($(window).scrollTop() + $(window).height() >= $(document).height() / 2) {
+            if (getCookie(modalId) == "") {
+                $("#" + modalId).modal("show");
+                if (navigator.userAgent.indexOf("MSIE") > 0) {
+                    createCookie(modalId, 'displayed', 1, false);
+                } else {
+                    createCookie(modalId, 'displayed', -1, true);
+                }
+            }
+        }
+    });
+});
