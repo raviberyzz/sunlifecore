2c04d4a8169ca83d80be3fa7bc92943b5aa2468d
Kateryna Semenova <ad01@clarica.com>
2024-03-05 13:56:20 -0500

    Added throttle to the scroll function to limit amount of calls. Added JSDoc comments MP-1329

--- ui.frontend/src/main/webpack/components/content/core-modal/v1/modal/index.js
+++ ui.frontend/src/main/webpack/components/content/core-modal/v1/modal/index.js
@@ -1,11 +1,26 @@
-/**
-* Display a 'triggerOnScroll' modal when scrolling halfway down the page.
-* Display the modal only once per browser session, managed by reading a cookie.
-*/
 $(document).ready(function () {
-    $(window).scroll(function () {
-        let modalTriggerOnScroll = document.querySelectorAll('[data-triggerOnScroll]')[0];
-        if (modalTriggerOnScroll != undefined)
+
+    /**
+     * Set up throttler.
+     * @function
+     * @param {function} fn - The function to throttle.
+     * @param {number} delay - The function will run once per given amount of milliseconds.
+     */
+    const throttle = (fn, delay) => {
+        let time = Date.now();
+        return () => {
+            if ((time + delay - Date.now()) <= 0) {
+                fn();
+                time = Date.now();
+            }
+        }
+    }
+
+    /**
+     * Trigger the modal to display. Display the modal only once per browser session, managed by reading a cookie.
+     * @function
+     */
+    function triggerModal() {
         var modalId = modalTriggerOnScroll.getAttribute('id');
         if ($(window).scrollTop() + $(window).height() >= $(document).height() / 2) {
             if (getCookie(modalId) == "") {
@@ -15,7 +30,14 @@ $(document).ready(function () {
                 } else {
                     createCookie(modalId, 'displayed', -1, true);
                 }
+            } else {
+                window.removeEventListener('scroll', throttle(triggerModal, 300));
             }
         }
-    });
+    }
+    
+    var modalTriggerOnScroll = document.querySelectorAll('[data-triggerOnScroll]')[0];
+    if (modalTriggerOnScroll != undefined) {
+        window.addEventListener('scroll', throttle(triggerModal, 300));
+    }
 });
