590463a5b44a5850651aef372db075167eb6c877
Amogh Aggarwal <amogh.aggarwal@sunlife.com>
2024-03-06 18:13:40 +0530

    MP-2506 : Implemented the showhide functionality for form-text component

---
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/form/core-text/v1/text/clientlibs-author/js/icon-showhide.js
@@ -0,0 +1,23 @@
+(function(document, $) {
+    "use strict";
+ 
+    $(document).on("foundation-contentloaded", function (e) {
+        Coral.commons.ready(function () {
+            showHideHandler($('.coral-Form-field[name*="./type"]', e.target));
+        });
+    });
+
+    $(document).on("change", '.coral-Form-field[name*="./type"]', function(e) {
+        showHideHandler($(this));
+    });
+ 
+    function showHideHandler(el) {
+        console.log(el.val());
+        if(el.val() === "textarea"){
+            el.parent(".coral-Form-fieldwrapper").siblings(".showhide-icon").first().addClass("hide");
+        }
+        else{
+            el.parent(".coral-Form-fieldwrapper").siblings(".showhide-icon").first().removeClass("hide");
+        }
+    }
+})(document, Granite.$);
