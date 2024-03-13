bad91937ff1d0260861015591935c8090253f339
OD22 <Sravani.Bammidi@sunlife.com>
2024-03-13 13:31:56 -0400

    styling fixes for text field

--- ui.frontend/src/main/webpack/components/form/core-text/v1/text/index.js
+++ ui.frontend/src/main/webpack/components/form/core-text/v1/text/index.js
@@ -1,24 +1,11 @@
 /**
- * Text field validation on key up key press.
- * Text area validation on key up key press.
- * Updating entered text count for text area on key up
+* To update the text count in text area
 */
 (function () {
     /**
-         * Function use for field validation on keyup
-         * @function
-    */
-    function fieldValidationKeyEventHandler() {
-        const $field = $(this);
-        const $fieldContainer = $field.parents(".form-floating");
-        $fieldContainer.toggleClass("sl-input-error", $field.val() === ' ');
-        if($fieldContainer.hasClass('sl-text-area')){
-            $fieldContainer.find(".character-count .count").html($field.val().length)
-        }
-    }
-    /**
-         * Function use for field validation on key press
-         * @function
+     * Function use for field validation on key press
+     * @function 
+     * @returns {} It will not return anything, this method just update DOM
     */
     function fieldValidationKeyPressEventHandler() {
         const $field = $(this);
@@ -28,10 +15,11 @@
             $fieldContainer.find(".visually-hidden .count").html($field.val().length+1);            
         }
     }
-    /**
-         * Check if the dropdown component exists.
-         * @function
-         * @field {object} scope - scope of the selected element
+     /**
+     * Check if a dropdown component exists.
+     * @function
+     * @param {object} $field - scope of the dom element
+     * @returns {} It will not return anything, this method just update DOM
     */
     function doesModuleExist($field) {
         if ($field.length <= 0) {
@@ -39,16 +27,14 @@
         }
         return true;
     }
-    /**
-         * Function use to initilize the event
-         * @function
+     /**
+     * Function used to initilize the event
+     * @function
+     * @returns {} It will not return anything, this method just update DOM
     */
     function init() {
         let $field = $(".sl-text-field .form-control.required, .sl-text-area .form-control.required");
         let $fieldForCount = $(".sl-text-area .form-control")
-        if(doesModuleExist($field)){
-            $field.keyup(fieldValidationKeyEventHandler);
-        }
         if(doesModuleExist($field) && $fieldForCount){
             $fieldForCount.keypress(fieldValidationKeyPressEventHandler);
         }
