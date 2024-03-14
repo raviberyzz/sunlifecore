512dfd331b9f86463442e2a2a8261803594ca32a
VF52 <deepak.chauhan@sunlife.com>
2024-03-15 00:10:20 +0530

    MP-2606 : Updated the show-hide script.

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-modal/v1/modal/clientlibs-author/js/tab-show-hide.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-modal/v1/modal/clientlibs-author/js/tab-show-hide.js
@@ -2,7 +2,9 @@
     "use strict";
     
     $(document).on("foundation-contentloaded", function(e) {
-        showHideHandler($(".cq-dialog-tab-showhide", e.target));
+        setTimeout(function(){
+            showHideHandler($(".cq-dialog-tab-showhide", e.target));
+        }, 10);
     });
 
     $(document).on("change", ".cq-dialog-tab-showhide", function(e) {
@@ -14,14 +16,14 @@
         var selectedValue = el.val();
 
         if (selectedValue == 'standardModal') {
-            $("coral-tab-label:contains('Footer Settings')").parent().prop("style","display: block;");
+            $("coral-tab-label:contains('Modal buttons')").parent().prop("style","display: block;");
             if(!$("div.text-option-showhide-target").hasClass("hide")) {
 				$(".text-option-showhide-target").addClass("hide");
             }
 
         }
         else if(selectedValue == 'formModal') {
-			$("coral-tab-label:contains('Footer Settings')").parent().hide();
+			$("coral-tab-label:contains('Modal buttons')").parent().hide();
 			$(".text-option-showhide-target").removeClass("hide");
         }
     }
