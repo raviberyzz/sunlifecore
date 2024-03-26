031d24ab013dac7ee1a9fcc15455beb4b47ce55d
ML\vb30 <amit.chauhan@sunlife.com>
2024-03-27 00:41:12 +0530

    MP-2671 : code updated for Search Result page now French language keyword can be author for Filter options.

---
+++ core/src/main/java/ca/sunlife/web/cms/core/models/v1/SearchResultsModel.java
@@ -0,0 +1,35 @@
+package ca.sunlife.web.cms.core.models.v1;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.List;
+
+
+@Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+
+public interface SearchResultsModel {
+
+    @ValueMapValue
+    String getFilterHeading();
+
+    @ValueMapValue
+    String getSearchResults();
+
+    @ChildResource
+    List<FilterOptions> filterOptions();
+
+    @Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+    interface FilterOptions {
+
+        @ValueMapValue
+        String getDataID();
+
+        @ValueMapValue
+        String getDataCaption();
+    }
+}
+ 
