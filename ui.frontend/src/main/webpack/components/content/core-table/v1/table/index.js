b4ad0669f0a35ec5297e17c948fdd177dbc85511
OD22 <Sravani.Bammidi@sunlife.com>
2024-04-02 15:03:44 -0400

    Refactored JS for table and fixed word wrapping

--- ui.frontend/src/main/webpack/components/content/core-table/v1/table/index.js
+++ ui.frontend/src/main/webpack/components/content/core-table/v1/table/index.js
@@ -1,8 +1,30 @@
-$(document).ready(function () {
-    //Function is used to update the table class in all table element inside sl-table class
-     function init() {
-        const $table = $(".sl-table table");
+
+//Function is used to update the table class in all table element inside sl-table class
+(function (core) {
+	"use strict";
+
+	/**
+	 * Text component
+	 * @namespace coreTable
+	 * @memberof sunCore.comp
+	 */
+    core.comp.coreTable = (function ($, util) {
+    const CONSTANT = {
+        SELECTOR: {
+        table: '.sl-table table"',
+        }
+    };
+    function init() {
+        const $table = $(CONSTANT.SELECTOR.table);
         $table?.addClass("table")
-    }
-    init();
-});
+    }   
+    return {
+      init: init,
+  };
+  })(core.$, core.util);
+  
+  /**
+  * Initialise coreTable module if given selector is in DOM
+  */
+  core.util.initialise(core.comp, "coreTable", ".sl-table");
+  })(sunCore);
