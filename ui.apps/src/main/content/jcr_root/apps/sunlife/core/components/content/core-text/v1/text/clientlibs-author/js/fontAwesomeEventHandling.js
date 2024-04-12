7f42ddf7c655746bd04161c70eeaf2c063ba85dd
Andrew Skibicki <Andrew.Skibicki@sunlife.com>
2024-04-12 12:40:09 -0400

    Updated

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-text/v1/text/clientlibs-author/js/fontAwesomeEventHandling.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-text/v1/text/clientlibs-author/js/fontAwesomeEventHandling.js
@@ -1,48 +1,66 @@
+/**
+ * fontAwesomeEventHandler.js
+ * @fileOverview Handles focus and blur events on the RTE editor. Inserts a '&nbsp;' character after a fontAwesome icon on focus event 
+ * and removes it on the blur event to allow for font selection.
+ */
 (function ($) {
     "use strict";
 
-    //when rich text editor is in focus, add a space after any fontAwesome <svg> or <span> tags to allow for font selection
+    /**
+     * When rich text editor is in focus, add a space after any fontAwesome <svg> or <span> tag.
+     */
     $(document).on('focus','.cq-RichText-editable', function(){
         let svgElement = $('.cq-RichText-editable').find('svg:last');
         if($(svgElement).parent().get(0)){
-			let svgParent =  $(svgElement).parent().get(0);
-            let svgParentInnerHTML = svgParent.innerHTML;
-            if(svgParentInnerHTML.indexOf("</svg>") > -1 ){
-                $(svgParent).html(svgParentInnerHTML.replace(/<\/svg>/g, '</svg>&nbsp;'));
-            }
+            modifySpaceAfterElement(svgElement,"</svg>","<\\/svg>","insert");
         }
         let spanElement = $('.cq-RichText-editable').find('span:last');
         if(spanElement.hasClass('font-awesome-icon')){
-			let spanParent =  $(spanElement).parent().get(0);
-            if(spanParent.innerHTML){
-                let spanParentInnerHTML = spanParent.innerHTML;
-                if(spanParentInnerHTML.indexOf("</span>") > -1 ){
-                    $(spanParent).html(spanParentInnerHTML.replace(/<\/span>/g, '</span>&nbsp;'));
-                }
-            }
+            modifySpaceAfterElement(spanElement,"</span>", "<\\/span>","insert");
         }
     });
 
-    //when rich text editor is blurred, remove any added space after <svg> or <span> tags
+    /**
+     * when rich text editor is blurred, remove any added space after <svg> or <span> tags.
+     */
     $(document).on('blur','.cq-RichText-editable', function(){
  		let svgElement = $('.cq-RichText-editable').find('svg:last');
         if($(svgElement).parent().get(0)){
-			let svgParent =  $(svgElement).parent().get(0);
-            let svgParentInnerHTML = svgParent.innerHTML;
-            if(svgParentInnerHTML.indexOf("</svg>&nbsp;") > -1 ){
-                $(svgParent).html(svgParentInnerHTML.replace(/<\/svg>&nbsp;/g, '</svg>'));
-            }
+            modifySpaceAfterElement(svgElement,"</svg>","<\\/svg>","remove");
         }
 		let spanElement = $('.cq-RichText-editable').find('span:last');
         if(spanElement.hasClass('font-awesome-icon')){
-			let spanParent =  $(spanElement).parent().get(0);
-            if(spanParent.innerHTML){
-                let spanParentInnerHTML = spanParent.innerHTML;
-                if(spanParentInnerHTML.indexOf("</span>&nbsp;") > -1 ){
-                    $(spanParent).html(spanParentInnerHTML.replace(/<\/span>&nbsp;/g, '</span>'));
-                }
-            }
+            modifySpaceAfterElement(spanElement,"</span>", "<\\/span>","remove");
         }
  	});
+
+    /**
+    * Function to add or remove a space after an <svg> or <span> tag.   
+    * @param {String} element Target element
+    * @param {String} endTag The end tag to modify
+    * @param {String} expression Regex expression
+    * @param {String} action The Action to execute; either 'insert' or 'remove'
+    * @return void
+    */
+
+    function modifySpaceAfterElement(element, endTag, expression, action){
+		let elementParent =  $(element).parent().get(0);
+		if(elementParent.innerHTML){
+			let elementParentInnerHTML = elementParent.innerHTML;
+			let suffix = "&nbsp;";
+			if(action === 'remove'){
+				endTag = endTag + suffix;
+				expression = expression + suffix;
+				suffix = "";
+			}
+			if(elementParentInnerHTML.indexOf(endTag) > -1 ){
+				let regex = new RegExp(expression, 'g');
+				if(action === 'remove'){
+                   endTag = endTag.replace("&nbsp;","");
+				}
+				$(elementParent).html(elementParentInnerHTML.replace(regex, endTag+suffix));
+			}
+		}
+    }
 
 })(Granite.$, Granite);
