36a8b619c86214d6942aa82bca58ab36e1ef1573
TZ38 <siva.rama.krishna@sunlife.com>
2024-04-15 13:40:16 -0400

    form container code refactor

--- ui.frontend/src/main/webpack/components/form/core-container/v1/container/index.js
+++ ui.frontend/src/main/webpack/components/form/core-container/v1/container/index.js
@@ -1,50 +1,144 @@
 /**
    * Handle Parsley custom error element container for dropdowns.
 */
-$(function () {
-  const errorsWrapperHtml = '<div id="error-helper-text" class="sl-helper-text error-text combo-msg"><span class="fak fa-exclamation-triangle sl-icon_size_sm sl-icon_color_error sl-icon_non-interactive"></span></div>';
-  const errorTemplateHtml = '<span></span>';
-  const parsleyConfig = {
-    errorsContainer: function (elem) {
-      return elem.$element.next('.error-text');
-    }, errorsWrapper: errorsWrapperHtml,
-    errorTemplate: errorTemplateHtml
-  };
-  if ($('form').length > 0) {
-    $('form').parsley(parsleyConfig);
-  }    
-});
+ 
+(function (core) {
+  "use strict";
+ 
+  /**
+   * Form Container component
+   * @namespace formContainer
+   * @memberof sunCore.comp
+   */
+    core.comp.formContainer = (function ($, util) {
+    const CONSTANT = {
+      SELECTOR: {
+        form: 'form',
+        fieldError: 'field:error',
+        fieldSuccess: 'field:error',
+        formError: 'form:error',
+        formValidate: '.formValidate',
+        parsleyCustomErrorMessage: '.parsley-custom-error-message',
+        formField: '.form-field',
+        errorField: '.sl-helper-text.error-text.filled',
+        errorText: '.error-text',
+        slDropdown: '.sl-dropdown',
+        comboInput: '.combo-input'
+      },
+      CLASS: {
+        slInputError: 'sl-input-error'
+      },
+      ATTR: {
+        ariaDescribedby: 'aria-describedby',
+        ariaInvalid: 'aria-invalid',
+        role: 'role',
+        dataParsleyPatternMessage: 'data-parsley-pattern-message'
+      },
+      ELEMENT:{
+        errorMsg: '<div id="error-helper-text" class="sl-helper-text error-text combo-msg"><span class="fak fa-exclamation-triangle sl-icon_size_sm sl-icon_color_error sl-icon_non-interactive"></span></div>',
+        span: '<span></span>'
+      }
+    };
+    /**
+     * Method to defined form config
+     * @function formConfig
+     * @memberof sunCore.comp.formContainer
+     * @private
+     */
+    function formConfig() {
+      const errorsWrapperHtml = CONSTANT.ELEMENT.errorMsg;
+      const errorTemplateHtml = CONSTANT.ELEMENT.span;
+      const parsleyConfig = {
+        errorsContainer: function (elem) {
+          return elem.$element.next(CONSTANT.SELECTOR.errorText);
+        }, errorsWrapper: errorsWrapperHtml,
+        errorTemplate: errorTemplateHtml
+      };
+      if ($(CONSTANT.SELECTOR.form).length > 0) {
+        $(CONSTANT.SELECTOR.form).parsley(parsleyConfig);
+      }
+    }
+    /**
+     * Handler to add input error styles to text, options and dropdowns.
+     * @function formFieldSuccess
+     * @memberof sunCore.comp.formContainer
+     * @private
+    */
+    function formFieldError() {
+      let $input = $(this.$element[0].closest(CONSTANT.SELECTOR.formValidate)).find(CONSTANT.SELECTOR.formField);
+      $input.addClass(CONSTANT.CLASS.slInputError);
+      this.$element.each(function(){
+        $(this).closest(CONSTANT.SELECTOR.formValidate).addClass(CONSTANT.CLASS.slInputError);
+      })
+      let $errorTextId = $(this.$element[0].closest(CONSTANT.SELECTOR.formValidate)).find(CONSTANT.SELECTOR.errorField)[0].getAttribute("id");
+      $input[0].setAttribute(CONSTANT.ATTR.ariaDescribedby, $errorTextId);
+      $input[0].setAttribute(CONSTANT.ATTR.ariaInvalid, "true");
+      if(this.$element[0]?.type === "text" && this.$element[0]?.value !== ""){
+        const patternError = $(this.$element[0]).attr(CONSTANT.ATTR.dataParsleyPatternMessage);
+        $(this.$element[0].closest(CONSTANT.SELECTOR.formValidate)).find(CONSTANT.SELECTOR.parsleyCustomErrorMessage).html(patternError);      
+      }    
+    }
+    /**
+     * Handler to input success styles to text, options and dropdowns.
+     * @function formFieldSuccess
+     * @memberof sunCore.comp.formContainer
+     * @private
+    */
+    function formFieldSuccess() {
+      this.$element.each(function(){
+        $(this).closest(CONSTANT.SELECTOR.formValidate).removeClass(CONSTANT.CLASS.slInputError);
+      })
+    }    
+    /**
+     * Handler to Focus on the first dropdown with an error after form submission.
+     * @function formValidationError
+     * @memberof sunCore.comp.formContainer
+     * @private
+    */    
+    function formValidationError() {
+      let $firstErrorText = $(this.$element[0]).find(CONSTANT.SELECTOR.parsleyCustomErrorMessage).first()[0];
+      $firstErrorText.setAttribute(CONSTANT.ATTR.role, "alert");
+      $($firstErrorText.closest(CONSTANT.SELECTOR.slDropdown)).find(CONSTANT.SELECTOR.comboInput).focus();
+    }
+    /**
+     * Handler to bind event specific for formContainer
+     * @function bindEvent
+     * @memberof sunCore.comp.formContainer
+     * @private
+     */
+    function bindEvent() {
+      $(document).on(
+        $(window.Parsley(on)),
+        CONSTANT.SELECTOR.fieldError,
+        formFieldError
+      );
+      $(document).on(
+        $(window.Parsley(on)),
+        CONSTANT.SELECTOR.fieldSuccess,
+        formFieldSuccess
+      );
+      $(document).on(
+        $(window.Parsley(on)),
+        CONSTANT.SELECTOR.formError,
+        formValidationError
+      );
+    }
+        /**
+    * Method used to initilize the module
+    * @function init
+    * @memberof sunCore.comp.formContainer
+    * @public
+    */
+    function init() {
+      formConfig();
+      bindEvent();
+    }        
+    return {
+        init: init,
+    };
+})(core.$, core.util);
 /**
- * On input error add error styles to text, options and dropdowns.
- * @function
-*/
-window.Parsley.on('field:error', function () {
-  let $input = $(this.$element[0].closest(".formValidate")).find(".form-field");
-  $input.addClass("sl-input-error");
-  this.$element.each(function(){
-    $(this).closest(".formValidate").addClass("sl-input-error");
-  })
-  let $errorTextId = $(this.$element[0].closest(".formValidate")).find(".sl-helper-text.error-text.filled")[0].getAttribute("id");
-  $input[0].setAttribute("aria-describedby", $errorTextId);
-  $input[0].setAttribute("aria-invalid", "true");
-  if(this.$element[0]?.type === "text" && this.$element[0]?.value !== ""){
-    const patternError = $(this.$element[0]).attr("data-parsley-pattern-message");
-    $(this.$element[0].closest(".formValidate")).find(".parsley-custom-error-message").html(patternError);      
-  } 
-}).on('field:success', function () {
-  this.$element.each(function(){
-    $(this).closest(".formValidate").removeClass("sl-input-error");
-  })
-});
-
-/**
- * Focus on the first dropdown with an error after form submission.
- * @function
-*/
-window.Parsley.on('form:validate', function (formInstance) {
-}).on('form:error', function () {
-  let $firstErrorText = $(this.$element[0]).find(".parsley-custom-error-message").first()[0];
-  $firstErrorText.setAttribute("role", "alert");
-  $($firstErrorText.closest(".sl-dropdown")).find(".combo-input").focus();
-}); 
-
+ * Initialise formContainer module if given selector is in DOM
+ */
+core.util.initialise(core.comp, "formContainer", ".formValidate");
+})(window, window.sunCore);
