8ecbefd4152c8a71c77b600f85f69e2bcae791f4
Amogh Aggarwal <amogh.aggarwal@sunlife.com>
2024-04-15 20:25:02 +0530

    Edited comments to follow convention.

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/form/core-options/v1/options/clientlibs-author/js/hide-checkbox.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/form/core-options/v1/options/clientlibs-author/js/hide-checkbox.js
@@ -2,28 +2,22 @@
     "use strict"
     $(document).on('foundation-contentloaded', function(event) {
         checkToHideCheckbox();
-        /** 
-         * Calling method to disable/enable checkbox on dialog load
-         */
+        /** Calling method to disable/enable checkbox on dialog load **/
         let $showhideTarget = $('.list-option-listfrom-showhide-target');
         $showhideTarget.on('click', '.coral3-Checkbox-input[name*="./selected"]', delayHideCheckbox);
         $showhideTarget.on('click', 'button[coral-multifield-add]', delayHideCheckbox);
         $(document).on('click', "button[class*='coral3-Multifield-remove']", delayHideCheckbox);
     });
+	
+	/** A small delay function to allow time for dialogue to load completely before running script **/
 
     function delayHideCheckbox() {
-        /**
-         * Adding small delay to function to prevent function running before new field is added
-         */
         setTimeout(
             function() {
                 checkToHideCheckbox();
             }, 10);
     }
-    /**
-     * This function will pick the appropriate checkboxes to hide and show
-     * when any change is made in the multifield like add, remove, check or uncheck
-     */
+    /** This function will pick the appropriate checkboxes to hide and show when any change is made in the multifield like add, remove, check or uncheck **/
     function checkToHideCheckbox(event) {
 
         if ($('.coral3-Select[name*="./type"]').val() == "radio") {
