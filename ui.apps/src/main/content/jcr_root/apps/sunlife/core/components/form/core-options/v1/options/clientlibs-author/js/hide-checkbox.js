a98460a867f0ab7467c94e45c788f4d78f723435
Amogh Aggarwal <amogh.aggarwal@sunlife.com>
2024-04-17 22:11:04 +0530

    Comments Updated

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/form/core-options/v1/options/clientlibs-author/js/hide-checkbox.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/form/core-options/v1/options/clientlibs-author/js/hide-checkbox.js
@@ -2,38 +2,35 @@
     "use strict"
     $(document).on('foundation-contentloaded', function(event) {
         checkToHideCheckbox();
-        /** Calling method to disable/enable checkbox on dialog load **/
+        // Calling method to disable/enable checkbox on dialog load
         let $showhideTarget = $('.list-option-listfrom-showhide-target');
         $showhideTarget.on('click', '.coral3-Checkbox-input[name*="./selected"]', delayHideCheckbox);
         $showhideTarget.on('click', 'button[coral-multifield-add]', delayHideCheckbox);
         $(document).on('click', "button[class*='coral3-Multifield-remove']", delayHideCheckbox);
     });
-	
-	/** 
-	 * @function A small delay function to allow time for dialogue to load completely before running script
-	 * @param No arguments needed
-	 * @return Doesn't return anything but calls another function
-	 */
+
+    /** 
+     * @function A small delay function to allow time for dialogue to load completely
+     * before running script
+     */
     function delayHideCheckbox() {
         setTimeout(
             function() {
                 checkToHideCheckbox();
             }, 10);
     }
+
     /** 
-	 * @function This function will pick the appropriate checkboxes to hide and show when any change is made in the multifield like add, remove, check or uncheck
-	 * @param No arguments needed
-	 * @return Doesn't return anything
-	 */
+     * @function This function will pick the appropriate checkboxes to hide and show when any
+     * change is made in the multifield like add, remove, check or uncheck
+     */
     function checkToHideCheckbox() {
 
         if ($('.coral3-Select[name*="./type"]').val() == "radio") {
             var $selectCheckboxes = $('.coral3-Checkbox-input[name*="./selected"]');
             var $selectedCheckbox = $('.coral3-Checkbox[checked]');
 
-			/** 
-			 * @this Picking each checbox and its parent div to hide or show depending on if-else clause
-			 */
+            // @this Picking each checbox and its parent div to hide or show depending on if-else clause
             $selectCheckboxes.each(function() {
                 $(this).parent().removeClass("hide");
             });
