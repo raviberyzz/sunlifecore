240c357b7f8f2753876d3f39c8c3e6395170b49f
Johncarlo Spilotro <johncarlo.spilotro@sunlife.com>
2024-05-02 11:12:13 -0400

    MP-2886 MP-2910 - Accordion Accessibility Fixes

--- ui.frontend/src/main/webpack/components/content/core-accordion/v1/accordion/index.js
+++ ui.frontend/src/main/webpack/components/content/core-accordion/v1/accordion/index.js
@@ -2,7 +2,6 @@
  * accordion.js
  * accordion related functionality.
  * This module used to handle the accordion click and key event.
- * Handle accordion default selection.
  * Handler to reset accordion for single selection.
  */
 (function (core) {
@@ -25,8 +24,7 @@
       accordionBody: '.accordion-body',
       accordionItem: '.accordion-item',
       accordionButton: '.accordion-button',
-      slIcon: '.sl-icon',
-      accordionCollapseExpanded: '.accordion-collapse.show.expanded'
+      slIcon: '.sl-icon'
     },
     CLASS: {
       show: 'show',
@@ -36,6 +34,7 @@
       showExpanded: 'show expanded'
     },
     ATTR: {
+      ariaexpanded: 'aria-expanded',
       dataSingleExpansion: 'data-single-expansion',
       height: 'height'
     }
@@ -60,39 +59,6 @@
     }
   }
   /**
-   * Method to handle the arrow key event to focus the accordion item and content
-   * @function tabArrowKeyFocusOnContent
-   * @memberof sunCore.comp.accordion
-   * @private
-   * @param {object} event - event object
-   * @param {object} $scope - scope of selected element
-  */
-  function tabArrowKeyFocusOnContent(event, $scope) {
-    const accordionContent = $scope.siblings(".accordion-collapse");
-    const accordionItemHeader = $scope.siblings(".accordion-header").find("button");
-    if(event.keyCode === util.constants.KeyCode.DOWN){
-      accordionContent.attr('tabindex', "0");
-      accordionContent.focus();
-    }
-    if(event.keyCode === util.constants.KeyCode.UP){
-      $scope.removeAttr('tabindex');
-      accordionItemHeader.focus();
-    }
-  }
-  /**
-   * Method to handle the arrow key event to focus the accordion item
-   * @function tabArrowKeyFocusOnItem
-   * @memberof sunCore.comp.accordion
-   * @private
-   * @param {object} e - event object
-  */
-  function tabArrowKeyFocusOnItem(e) {
-    const $scope = $(this);
-    if ($(this).hasClass(CONSTANT.CLASS.expanded)) {
-      tabArrowKeyFocusOnContent(e, $scope);
-    }
-  }
-  /**
    * Method to handle the escape key event to close the accordion on escape
    * @function accordionHeaderKeyEventHandler
    * @memberof sunCore.comp.accordion
@@ -101,24 +67,11 @@
   */
   function accordionHeaderKeyEventHandler(e) {
     const $scope = $(this);
-    if (e.keyCode === util.constants.KeyCode.ESC && $(this).siblings(CONSTANT.SELECTOR.accordionCollapse).hasClass(CONSTANT.CLASS.expanded)) {
-      toggleAccordionCollapse($(this));
-    }
-    if ($(this).siblings(CONSTANT.SELECTOR.accordionCollapse).hasClass(CONSTANT.CLASS.expanded)) {
-      tabArrowKeyFocusOnContent(e, $scope);
-    }
-  }
-  /**
-   * Method to update the height of accordion item to apply smooth csss animation. 
-   * @function updateAccordionItemHeight
-   * @memberof sunCore.comp.accordion
-   * @private
-   * @param {object} $accordionContentElement - scope each accordion content element
-  */
-  function updateAccordionItemHeight($accordionContentElement){
-    setTimeout(function(){
-      $accordionContentElement.height(($accordionContentElement.find(CONSTANT.SELECTOR.accordionBody).outerHeight()) + 24 + "px");
-    },10)
+    if (e.keyCode === util.constants.KeyCode.ESC && $scope.siblings(CONSTANT.SELECTOR.accordionCollapse).hasClass(CONSTANT.CLASS.show)) {
+      const accordionCollapse = new bootstrap.Collapse($scope.siblings(CONSTANT.SELECTOR.accordionCollapse));
+      accordionCollapse.toggle();
+      chevronHandler($scope, true);
+    }    
   }
   /**
    * Function used to reset the accordion if the single Selection is true in authoring
@@ -130,8 +83,9 @@
   function resetAccordionForSingleSelection($accordionItemHeader) {
     const $singleExpansion = $accordionItemHeader.attr(CONSTANT.ATTR.dataSingleExpansion);
     if ($singleExpansion == "true") {
+      $accordionItemHeader.find(CONSTANT.SELECTOR.accordionButton).attr(CONSTANT.ATTR.ariaexpanded, "true");
       const $accordionItem = $accordionItemHeader.parents(CONSTANT.SELECTOR.accordionItem).parents(CONSTANT.SELECTOR.accordion);
-      $accordionItem.find(CONSTANT.SELECTOR.accordionCollapse).removeClass(CONSTANT.CLASS.showExpanded).css(CONSTANT.ATTR.height, "").attr("aria-hidden", true);
+      $accordionItem.find(CONSTANT.SELECTOR.accordionCollapse).removeClass(CONSTANT.CLASS.showExpanded).css(CONSTANT.ATTR.height, "")
       $accordionItem.find(CONSTANT.SELECTOR.accordionButton).addClass(CONSTANT.CLASS.collapsed);
       $accordionItem.find(CONSTANT.SELECTOR.slIcon).removeClass(CONSTANT.CLASS.show).removeClass(CONSTANT.CLASS.hide);
       chevronHandler($accordionItem, true);
@@ -145,16 +99,12 @@
    */
   function accordionHeaderClickEventHandler() {
     const $accordionItemHeader = $(this);
-    const $accordionContentElement = $accordionItemHeader.siblings(CONSTANT.SELECTOR.accordionCollapse)
-    if ($accordionContentElement.hasClass(CONSTANT.CLASS.expanded)) {
-      toggleAccordionCollapse($accordionItemHeader);
-    } else {
+    if ($accordionItemHeader.find(CONSTANT.SELECTOR.accordionButton).attr(CONSTANT.ATTR.ariaexpanded) == "true") {
       resetAccordionForSingleSelection($accordionItemHeader);
-      $accordionItemHeader.find(CONSTANT.SELECTOR.slIcon).removeClass(CONSTANT.CLASS.hide).removeClass(CONSTANT.SELECTOR.show);
-      $accordionContentElement.addClass(CONSTANT.CLASS.showExpanded);
-      $accordionItemHeader.find(CONSTANT.SELECTOR.accordionButton).removeClass(CONSTANT.CLASS.collapsed);
       chevronHandler($accordionItemHeader, false);
-      updateAccordionItemHeight($accordionContentElement);
+    } else {
+      toggleAccordionCollapse($accordionItemHeader);
+      chevronHandler($accordionItemHeader, true);
     }
   }
   /**
@@ -166,42 +116,18 @@
    */
   function toggleAccordionCollapse($accordionHeader) {
     $accordionHeader.find(CONSTANT.SELECTOR.slIcon).removeClass(CONSTANT.CLASS.hide).removeClass(CONSTANT.CLASS.show);
-    $accordionHeader.find(CONSTANT.SELECTOR.accordionButton).addClass(CONSTANT.CLASS.collapsed);
-    $accordionHeader.siblings(CONSTANT.SELECTOR.accordionCollapse).removeClass(CONSTANT.CLASS.expanded).css(CONSTANT.ATTR.height, "");
-    chevronHandler($accordionHeader, true);
   }
   /**
-   * Functions used to select/toggle the default selected accordion items in authoring.
-   * @function accordionDefaultSelection
-   * @memberof sunCore.comp.accordion
-   * @private
-   */
-  function accordionDefaultSelection() {    
-    let $accordionContainer = $(CONSTANT.SELECTOR.accordionContainer)
-    $accordionContainer.each(function() {
-      const $accordion = $(this);
-      const $accordionExpandedItem = $accordion.find(CONSTANT.SELECTOR.accordionCollapseExpanded);
-      $accordionExpandedItem.each((function(index) {
-        updateAccordionItemHeight($accordionExpandedItem.eq(index));
-      }))      
-    })
-  }  
-  /**
-		 * Handler to bind event specific for accordion
-		 * @function bindEvent
-		 * @memberof sunCore.comp.accordion
-		 * @private
+     * Handler to bind event specific for accordion
+     * @function bindEvent
+     * @memberof sunCore.comp.accordion
+     * @private
   */
   function bindEvent() {
     $(document).on(
       util.customEvents.KEYDOWN,
       CONSTANT.SELECTOR.accordionHeaderElem,
       accordionHeaderKeyEventHandler
-    );
-    $(document).on(
-      util.customEvents.KEYDOWN,
-      CONSTANT.SELECTOR.accordionCollapse,
-      tabArrowKeyFocusOnItem
     );
     $(document).on(
       util.customEvents.INTERACTION,
@@ -216,7 +142,6 @@
     * @public
   */
   function init() {
-    accordionDefaultSelection();
     bindEvent();
   }  
   return {
