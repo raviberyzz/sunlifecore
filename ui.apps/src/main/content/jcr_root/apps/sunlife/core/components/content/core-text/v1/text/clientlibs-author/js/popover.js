bc69aacbfe954e9aab901d1625f805f2cec2887e
ML\og90 <andrew.castro@sunlife.com>
2024-05-08 13:32:45 -0400

    Revert "Making the title field required for popup to adhere to accessibility best practices."
    This reverts commit 077c9fb595983ae9cfb7a320974d9af45dfa05a8.

--- ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-text/v1/text/clientlibs-author/js/popover.js
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-text/v1/text/clientlibs-author/js/popover.js
@@ -24,7 +24,7 @@
                 +'<div class=" rte-dialog-columnContainer">'   
                 +'<div class=" rte-dialog-column">'
                 +'<label class="coral-Form-fieldlabel" id="label-aligned-textfield-0">Header Text : </label>'
-                +'<input class="coral-Form-field" is="coral-textfield" placeholder="Enter heading text" name="infoHead" value="" labelledby="label-aligned-textfield-0" aria-labelledby="label-aligned-textfield-0" required>'
+                +'<input class="coral-Form-field" is="coral-textfield" placeholder="Enter heading text" name="infoHead" value="" labelledby="label-aligned-textfield-0" aria-labelledby="label-aligned-textfield-0">'
                 +'</div>'
                 +'<div class=" rte-dialog-column">'
                 +'<label class="coral-Form-fieldlabel" id="label-aligned-textfield-0">Popover Content : </label>'
@@ -54,24 +54,18 @@
             var dialog = this.$dialog;
 
             this.$saveBtn.on('click',function(e){
-              var infoHead = dialog.find('[name="infoHead"]').val();
-              var infoDetail = dialog.find('[name="infoDetail"]').val(); 
-              if (!infoHead.trim() == '') {
+                var infoHead = dialog.find('[name="infoHead"]').val();
+                var infoDetail = dialog.find('[name="infoDetail"]').val(); 
                 config.parameters.saveData(infoHead, infoDetail);
                 dialog.hide();
                 e.preventDefault();
-              } else {
-                return;
-              }
-              
-              
             });
 
             this.$closeBtn.on('click',function() {
-              dialog.find('[name="infoHead"]').val('');
-              dialog.find('[name="infoDetail"]').val(''); 
-              dialog.hide();
-              e.preventDefault();
+                dialog.find('[name="infoHead"]').val('');
+                dialog.find('[name="infoDetail"]').val(''); 
+                dialog.hide();
+                e.preventDefault();
             });
             
         },
