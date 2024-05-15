3b7395f4848f74534515b96201c3fc5a96d1550b
TZ38 <siva.rama.krishna@sunlife.com>
2024-05-14 20:41:23 -0400

    MP-2836 & MP-2837 UX issues fixed

--- ui.frontend/src/main/webpack/prerequisite/core-utils/v1/scripts/constants.js
+++ ui.frontend/src/main/webpack/prerequisite/core-utils/v1/scripts/constants.js
@@ -54,10 +54,10 @@
             },
             templateString: {
                 externalLinkIcon: `<i class="far fa-external-link icn-hover-hidden" aria-hidden="false"></i>`,
-                internalLinkIcon: `<i class="far fa-arrow-circle-right icn-hover-hidden" aria-hidden="false"></i>`,
+                internalLinkIcon: `<i class="far fa-chevron-circle-right icn-hover-hidden" aria-hidden="false"></i>`,
                 pdfLinkIcon: `<i class="far fa-file-pdf icn-hover-hidden" aria-hidden="false"></i>`,
                 externalLinkIconSolid: `<i class="fas fa-external-link icn-hover-show" aria-hidden="false"></i>`,
-                internalLinkIconSolid: `<i class="fas fa-arrow-circle-right icn-hover-show" aria-hidden="false"></i>`,
+                internalLinkIconSolid: `<i class="fas fa-chevron-circle-right icn-hover-show" aria-hidden="false"></i>`,
                 pdfLinkIconSolid: `<i class="fas fa-file-pdf icn-hover-show" aria-hidden="false"></i>`
             },
         };
