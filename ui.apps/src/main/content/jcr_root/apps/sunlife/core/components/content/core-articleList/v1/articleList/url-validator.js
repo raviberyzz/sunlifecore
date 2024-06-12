56571b5537aae918bfb306b1f88a1801bc207775
ML\og90 <andrew.castro@sunlife.com>
2024-06-12 14:28:25 -0400

    Adding serverside JS logic using the JS use-api to extend the logic of what HTL can accomplish. Article List now passes the list of articles to the use-api to validate if the page is the same as one of the articles being rendered, it also caps the articles rendered at 3. This ensures the article will never link back to the article page and the list will be 3 items.

---
+++ ui.apps/src/main/content/jcr_root/apps/sunlife/core/components/content/core-articleList/v1/articleList/url-validator.js
@@ -0,0 +1,25 @@
+"use strict";
+
+use(function () {
+    let items = this.items;
+    let count = 0;
+    let newItems = [];
+    let page = currentPage.path;
+    items.forEach(item => {
+        if(count < 3){
+            item.elements.forEach(element => {
+                if (element.name == 'articlePageLink') {
+                    
+                    let link = element.value.split('.html').join('');
+                    if(link != page){
+                        newItems.push(item);
+                        count++;
+                    }
+                }
+            });
+        }
+    });
+    return {
+        newItems: newItems
+    }
+});
